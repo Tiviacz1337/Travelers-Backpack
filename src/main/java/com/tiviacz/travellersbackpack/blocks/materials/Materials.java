6b73baeb37e8f07d15511db6c764ee6031226b55
Tiviacz1337 <prusek.kamil@o2.pl>
2019-05-26 12:42:03 +0200

    New materials

---
+++ src/main/java/com/tiviacz/travellersbackpack/blocks/materials/Materials.java
@@ -0,0 +1,21 @@
+package com.tiviacz.travellersbackpack.blocks.materials;
+
+import net.minecraft.block.material.MapColor;
+import net.minecraft.block.material.Material;
+
+public class Materials extends Material
+{
+	public static final Materials BACKPACK = new Materials(MapColor.BROWN);
+	public static final Materials SLEEPING_BAG = new Materials(MapColor.RED);
+	
+	public Materials(MapColor color) 
+	{
+		super(color);
+	}
+	
+	@Override
+	public boolean blocksLight()
+    {
+        return false;
+    }
+}
--- src/main/java/com/tiviacz/travellersbackpack/init/ModBlocks.java
+++ src/main/java/com/tiviacz/travellersbackpack/init/ModBlocks.java
@@ -5,15 +5,15 @@ import java.util.List;
 
 import com.tiviacz.travellersbackpack.blocks.BlockSleepingBag;
 import com.tiviacz.travellersbackpack.blocks.BlockTravellersBackpack;
+import com.tiviacz.travellersbackpack.blocks.materials.Materials;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
 
 public class ModBlocks 
 {
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
 	
-	public static final Block TRAVELLERS_BACKPACK = new BlockTravellersBackpack("travellers_backpack", Material.CLOTH);
-	public static final Block SLEEPING_BAG_TOP = new BlockSleepingBag("sleeping_bag_top", Material.CLOTH);
-	public static final Block SLEEPING_BAG_BOTTOM = new BlockSleepingBag("sleeping_bag_bottom", Material.CLOTH);
+	public static final Block TRAVELLERS_BACKPACK = new BlockTravellersBackpack("travellers_backpack", Materials.BACKPACK);
+	public static final Block SLEEPING_BAG_TOP = new BlockSleepingBag("sleeping_bag_top", Materials.SLEEPING_BAG);
+	public static final Block SLEEPING_BAG_BOTTOM = new BlockSleepingBag("sleeping_bag_bottom", Materials.SLEEPING_BAG);
 }
