212f9f0eb378738f43d9b10a9381f71a9b1b5a54
Tiviacz1337 <prusek.kamil@o2.pl>
2019-06-10 15:15:25 +0200

    Comments

--- src/main/java/com/tiviacz/travellersbackpack/handlers/ConfigHandler.java
+++ src/main/java/com/tiviacz/travellersbackpack/handlers/ConfigHandler.java
@@ -3,25 +3,33 @@ package com.tiviacz.travellersbackpack.handlers;
 import com.tiviacz.travellersbackpack.TravellersBackpack;
 
 import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.Config.Comment;
 
 @Config(modid = TravellersBackpack.MODID)
 public class ConfigHandler 
 {
+	@Comment("Places backpack at place where player died")
 	public static boolean backpackDeathPlace = true;
 	
+	@Comment("Enables tool cycling via shift + scroll combination, while backpack is worn")
 	public static boolean enableToolCycling = true;
 	
 	public static boolean toolSlotsAcceptSwords = false;
 	
+	@Comment("Render tools in tool slots on the backpack, while worn")
 	public static boolean renderTools = true;
 	
+	@Comment("Enables tanks and tool slots overlay, while backpack is worn")
 	public static boolean enableOverlay = true;
 	
 	public static boolean oldGuiTankRender = false;
 	
+	@Comment("Enables wearing backpack directly from ground")
 	public static boolean enableBackpackBlockWearable = true;
 	
-//	public static boolean autoItemPickUp = true;
+	@Comment("Enables backpacks spawning in loot chests")
+	public static boolean enableLoot = true;
 	
-//	public static boolean canInteractWithOtherPlayersBackpack = true;
+	@Comment("Disabling this option may improve performance")
+	public static boolean enableBackpackItemFluidRenderer = true;
 }
