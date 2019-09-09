4f909e830c8699176400ff69283867206468d380
Tiviacz1337 <prusek.kamil@o2.pl>
2019-09-09 19:21:49 +0200

    Update SlotBackpack.java
    closes #44

--- src/main/java/com/tiviacz/travellersbackpack/gui/container/slots/SlotBackpack.java
+++ src/main/java/com/tiviacz/travellersbackpack/gui/container/slots/SlotBackpack.java
@@ -4,6 +4,7 @@ import com.tiviacz.travellersbackpack.init.ModItems;
 
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemShulkerBox;
 import net.minecraft.item.ItemStack;
 
 public class SlotBackpack extends Slot
@@ -16,6 +17,11 @@ public class SlotBackpack extends Slot
 	@Override
 	public boolean isItemValid(ItemStack stack)
     {
+		if(stack.getItem() instanceof ItemShulkerBox)
+		{
+			return false;
+		}
+		
         return stack.getItem() == ModItems.TRAVELLERS_BACKPACK ? false : true;
     }
 }
