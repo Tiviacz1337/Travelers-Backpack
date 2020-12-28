bd1514e6423f43270577fcd7086d58b921c7a6b2
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2020-12-28 02:27:43 +0100

    Version update

--- src/main/java/com/tiviacz/travelersbackpack/TravelersBackpack.java
+++ src/main/java/com/tiviacz/travelersbackpack/TravelersBackpack.java
@@ -19,7 +19,7 @@ public class TravelersBackpack
 {
     public static final String MODID = "travelersbackpack";
     public static final String NAME = "Traveler's Backpack";
-    public static final String VERSION = "1.0.34";
+    public static final String VERSION = "1.0.35";
     public static final String UPDATE_JSON = "https://gist.githubusercontent.com/Tiviacz1337/906937677aa472285dff9d6c2a189d5e/raw";
     public static final String CLIENT_PROXY_CLASS = "com.tiviacz.travelersbackpack.proxy.ClientProxy";
     public static final String COMMON_PROXY_CLASS = "com.tiviacz.travelersbackpack.proxy.CommonProxy";
