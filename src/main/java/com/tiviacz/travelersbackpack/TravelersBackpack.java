72232c2d7b30f6ffa624ad7aee43a21c631ecf52
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2022-06-22 19:49:35 +0200

    Registered sided fluid storage for backpack

--- src/main/java/com/tiviacz/travelersbackpack/TravelersBackpack.java
+++ src/main/java/com/tiviacz/travelersbackpack/TravelersBackpack.java
@@ -2,6 +2,7 @@ package com.tiviacz.travelersbackpack;
 
 import com.tiviacz.travelersbackpack.compat.trinkets.TrinketsCompat;
 import com.tiviacz.travelersbackpack.config.TravelersBackpackConfig;
+import com.tiviacz.travelersbackpack.fluids.EffectFluidRegistry;
 import com.tiviacz.travelersbackpack.handlers.EntityItemHandler;
 import com.tiviacz.travelersbackpack.handlers.LootHandler;
 import com.tiviacz.travelersbackpack.handlers.TradeOffersHandler;
@@ -10,10 +11,13 @@ import com.tiviacz.travelersbackpack.network.ModNetwork;
 import com.tiviacz.travelersbackpack.util.ResourceUtils;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class TravelersBackpack implements ModInitializer
 {
 	public static final String MODID = "travelersbackpack";
+	public static final Logger LOGGER = LogManager.getLogger();
 	private static boolean trinketsLoaded;
 
 	@Override
@@ -22,6 +26,7 @@ public class TravelersBackpack implements ModInitializer
 		ModBlocks.init();
 		ModItems.init();
 		ModBlockEntityTypes.init();
+		ModBlockEntityTypes.initSidedFluidStorage();
 		ModScreenHandlerTypes.init();
 		ModCrafting.init();
 		ModNetwork.initServer();
