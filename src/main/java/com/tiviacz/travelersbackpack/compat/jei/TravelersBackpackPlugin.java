10f03c74015d8a2291e8dcaa33e5e8fb6f5c2795
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2022-07-17 21:51:28 +0200

    ✨ JEI compat

--- src/main/java/com/tiviacz/travelersbackpack/compat/jei/TravelersBackpackPlugin.java
+++ src/main/java/com/tiviacz/travelersbackpack/compat/jei/TravelersBackpackPlugin.java
@@ -1,18 +1,23 @@
 package com.tiviacz.travelersbackpack.compat.jei;
 
 import com.tiviacz.travelersbackpack.TravelersBackpack;
+import com.tiviacz.travelersbackpack.init.ModMenuTypes;
 import com.tiviacz.travelersbackpack.inventory.container.TravelersBackpackBlockEntityMenu;
 import com.tiviacz.travelersbackpack.inventory.container.TravelersBackpackItemMenu;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.RecipeTypes;
+import mezz.jei.api.registration.IRecipeTransferRegistration;
 import net.minecraft.resources.ResourceLocation;
 
-/*@JeiPlugin
+@JeiPlugin
 public class TravelersBackpackPlugin implements IModPlugin
 {
     @Override
     public void registerRecipeTransferHandlers(IRecipeTransferRegistration registration)
     {
-        registration.addRecipeTransferHandler(TravelersBackpackItemMenu.class, VanillaRecipeCategoryUid.CRAFTING, 1, 9, 1, 90);
-        registration.addRecipeTransferHandler(TravelersBackpackBlockEntityMenu.class, VanillaRecipeCategoryUid.CRAFTING, 1, 9, 1, 90);
+        registration.addRecipeTransferHandler(TravelersBackpackItemMenu.class, ModMenuTypes.TRAVELERS_BACKPACK_ITEM.get(), RecipeTypes.CRAFTING, 1, 9, 10, 81);
+        registration.addRecipeTransferHandler(TravelersBackpackBlockEntityMenu.class, ModMenuTypes.TRAVELERS_BACKPACK_TILE.get(), RecipeTypes.CRAFTING, 1, 9, 10, 81);
     }
 
     @Override
@@ -20,4 +25,4 @@ public class TravelersBackpackPlugin implements IModPlugin
     {
         return new ResourceLocation(TravelersBackpack.MODID, "travelersbackpack");
     }
-} */
+}
