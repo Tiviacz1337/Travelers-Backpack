216b5c51e1b80ad738817c40d1f98b032ed77b70
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2022-07-17 21:53:39 +0200

    🐛 Fixed wrong registration of keybinds and overlay

--- src/main/java/com/tiviacz/travelersbackpack/handlers/ClientEventHandler.java
+++ src/main/java/com/tiviacz/travelersbackpack/handlers/ClientEventHandler.java
@@ -1,63 +1,27 @@
 package com.tiviacz.travelersbackpack.handlers;
 
-import com.mojang.blaze3d.vertex.PoseStack;
 import com.tiviacz.travelersbackpack.TravelersBackpack;
 import com.tiviacz.travelersbackpack.capability.CapabilityUtils;
-import com.tiviacz.travelersbackpack.client.gui.OverlayScreen;
 import com.tiviacz.travelersbackpack.config.TravelersBackpackConfig;
 import com.tiviacz.travelersbackpack.inventory.container.slot.ToolSlotItemHandler;
 import com.tiviacz.travelersbackpack.items.HoseItem;
 import com.tiviacz.travelersbackpack.items.TravelersBackpackItem;
-import com.tiviacz.travelersbackpack.network.SpecialActionPacket;
 import com.tiviacz.travelersbackpack.network.ScreenPacket;
+import com.tiviacz.travelersbackpack.network.SpecialActionPacket;
 import com.tiviacz.travelersbackpack.util.Reference;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.GameType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.InputEvent;
-import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
-import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
-import net.minecraftforge.client.gui.overlay.ForgeGui;
-import net.minecraftforge.client.gui.overlay.GuiOverlayManager;
-import net.minecraftforge.client.gui.overlay.IGuiOverlay;
-import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import org.lwjgl.glfw.GLFW;
 
 @Mod.EventBusSubscriber(modid = TravelersBackpack.MODID, value = Dist.CLIENT)
 public class ClientEventHandler
 {
-
-    public static final KeyMapping OPEN_INVENTORY = new KeyMapping(Reference.INVENTORY, GLFW.GLFW_KEY_B, Reference.CATEGORY);
-    public static final KeyMapping TOGGLE_TANK = new KeyMapping(Reference.TOGGLE_TANK, GLFW.GLFW_KEY_N, Reference.CATEGORY);
-    public static final KeyMapping CYCLE_TOOL = new KeyMapping(Reference.CYCLE_TOOL, GLFW.GLFW_KEY_Z, Reference.CATEGORY);
-
-    @SubscribeEvent
-    public static void registerKeys(final RegisterKeyMappingsEvent evt)
-    {
-        evt.register(OPEN_INVENTORY);
-        evt.register(TOGGLE_TANK);
-        evt.register(CYCLE_TOOL);
-    }
-
-    @SubscribeEvent
-    public static void registerKeys(final RegisterGuiOverlaysEvent evt)
-    {
-        evt.registerBelow(VanillaGuiOverlay.HOTBAR.id(), "Traveler's Backpack HUD", (gui, poseStack, partialTick, width, height) -> {
-            Minecraft mc = Minecraft.getInstance();
-
-            if(!mc.options.hideGui && CapabilityUtils.isWearingBackpack(mc.player) && mc.gameMode.getPlayerMode() != GameType.SPECTATOR)
-            {
-                OverlayScreen.renderOverlay(gui, mc, poseStack);
-            }
-        });
-    }
-
     @SubscribeEvent
     public static void clientTickEvent(final TickEvent.ClientTickEvent event)
     {
@@ -67,20 +31,20 @@ public class ClientEventHandler
 
         if(player != null && CapabilityUtils.isWearingBackpack(player))
         {
-            if(OPEN_INVENTORY.consumeClick())
+            if(ModClientEventsHandler.OPEN_INVENTORY.consumeClick())
             {
                 TravelersBackpack.NETWORK.sendToServer(new ScreenPacket(Reference.BACKPACK_GUI, Reference.FROM_KEYBIND));
             }
 
             if(player.getMainHandItem().getItem() instanceof HoseItem && player.getMainHandItem().getTag() != null)
             {
-                if(TOGGLE_TANK.consumeClick())
+                if(ModClientEventsHandler.TOGGLE_TANK.consumeClick())
                 {
                     TravelersBackpack.NETWORK.sendToServer(new SpecialActionPacket(0, Reference.TOGGLE_HOSE_TANK));
                 }
             }
 
-            KeyMapping key = CYCLE_TOOL;
+            KeyMapping key = ModClientEventsHandler.CYCLE_TOOL;
 
             if(TravelersBackpackConfig.disableScrollWheel && key.consumeClick())
             {
@@ -112,7 +76,7 @@ public class ClientEventHandler
     public static void mouseWheelDetect(InputEvent.MouseScrollingEvent event)
     {
         Minecraft mc = Minecraft.getInstance();
-        KeyMapping key1 = CYCLE_TOOL;
+        KeyMapping key1 = ModClientEventsHandler.CYCLE_TOOL;
         double scrollDelta = event.getScrollDelta();
 
         if(!TravelersBackpackConfig.disableScrollWheel && scrollDelta != 0.0)
--- src/main/java/com/tiviacz/travelersbackpack/handlers/ModClientEventsHandler.java
+++ src/main/java/com/tiviacz/travelersbackpack/handlers/ModClientEventsHandler.java
@@ -1,23 +1,57 @@
 package com.tiviacz.travelersbackpack.handlers;
 
 import com.tiviacz.travelersbackpack.TravelersBackpack;
+import com.tiviacz.travelersbackpack.capability.CapabilityUtils;
+import com.tiviacz.travelersbackpack.client.gui.OverlayScreen;
 import com.tiviacz.travelersbackpack.client.renderer.TravelersBackpackBlockEntityRenderer;
 import com.tiviacz.travelersbackpack.client.renderer.TravelersBackpackLayer;
+import com.tiviacz.travelersbackpack.util.Reference;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.GameType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.lwjgl.glfw.GLFW;
 
 @Mod.EventBusSubscriber(modid = TravelersBackpack.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModClientEventsHandler {
 
     public static final ModelLayerLocation TRAVELERS_BACKPACK_BLOCK_ENTITY = new ModelLayerLocation(new ResourceLocation(TravelersBackpack.MODID, "travelers_backpack"), "main");
     public static final ModelLayerLocation TRAVELERS_BACKPACK_WEARABLE = new ModelLayerLocation(new ResourceLocation(TravelersBackpack.MODID, "travelers_backpack"), "wearable");
+    public static final KeyMapping OPEN_INVENTORY = new KeyMapping(Reference.INVENTORY, GLFW.GLFW_KEY_B, Reference.CATEGORY);
+    public static final KeyMapping TOGGLE_TANK = new KeyMapping(Reference.TOGGLE_TANK, GLFW.GLFW_KEY_N, Reference.CATEGORY);
+    public static final KeyMapping CYCLE_TOOL = new KeyMapping(Reference.CYCLE_TOOL, GLFW.GLFW_KEY_Z, Reference.CATEGORY);
+
+    @SubscribeEvent
+    public static void registerKeys(final RegisterKeyMappingsEvent event)
+    {
+        event.register(OPEN_INVENTORY);
+        event.register(TOGGLE_TANK);
+        event.register(CYCLE_TOOL);
+    }
+
+    @SubscribeEvent
+    public static void registerOverlay(final RegisterGuiOverlaysEvent evt)
+    {
+        evt.registerBelow(VanillaGuiOverlay.HOTBAR.id(), "travelers_backpack", (gui, poseStack, partialTick, width, height) -> {
+            Minecraft mc = Minecraft.getInstance();
+
+            if(!mc.options.hideGui && CapabilityUtils.isWearingBackpack(mc.player) && mc.gameMode.getPlayerMode() != GameType.SPECTATOR)
+            {
+                OverlayScreen.renderOverlay(gui, mc, poseStack);
+            }
+        });
+    }
 
     @SubscribeEvent
     public static void layerDefinitions(EntityRenderersEvent.RegisterLayerDefinitions event)
