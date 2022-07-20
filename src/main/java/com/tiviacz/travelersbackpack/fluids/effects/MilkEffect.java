009527b335fbb6cb6ab79deb65e071e351af1ecb
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2022-07-20 18:44:22 +0200

    ✨ Milk Effect

---
+++ src/main/java/com/tiviacz/travelersbackpack/fluids/effects/MilkEffect.java
@@ -0,0 +1,33 @@
+package com.tiviacz.travelersbackpack.fluids.effects;
+
+import com.tiviacz.travelersbackpack.api.fluids.EffectFluid;
+import com.tiviacz.travelersbackpack.util.Reference;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.fluids.FluidStack;
+
+public class MilkEffect extends EffectFluid
+{
+    public MilkEffect()
+    {
+        super("minecraft", "milk", Reference.BUCKET);
+    }
+
+    @Override
+    public void affectDrinker(FluidStack fluidStack, Level level, Entity entity)
+    {
+        if(entity instanceof Player)
+        {
+            ((Player)entity).curePotionEffects(new ItemStack(Items.MILK_BUCKET));
+        }
+    }
+
+    @Override
+    public boolean canExecuteEffect(FluidStack stack, Level level, Entity entity)
+    {
+        return stack.getAmount() >= amountRequired;
+    }
+}
