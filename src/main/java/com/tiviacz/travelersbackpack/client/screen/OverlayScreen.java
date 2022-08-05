d1b60e93036e6e6fe17a0162f89e361535f3edfb
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2022-08-05 14:21:47 +0200

    🔧 Tools display durability

---
+++ src/main/java/com/tiviacz/travelersbackpack/client/screen/OverlayScreen.java
@@ -0,0 +1,3 @@
+            this.drawItemStack(inv.getInventory().getStackInSlot(Reference.TOOL_LOWER), scaledWidth - 30, scaledHeight + 11);
+        this.itemRenderer.renderGuiItem(stack, x, y);
+        itemRenderer.renderGuiItemDecorations(Minecraft.getInstance().font, stack, x, y);
