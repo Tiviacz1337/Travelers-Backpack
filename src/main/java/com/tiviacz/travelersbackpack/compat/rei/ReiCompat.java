52c1add5d17fbedaa635003b962c3e9ef859473f
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2022-10-13 23:20:47 +0200

    ✨ REI support

---
+++ src/main/java/com/tiviacz/travelersbackpack/compat/rei/ReiCompat.java
@@ -0,0 +1,91 @@
+package com.tiviacz.travelersbackpack.compat.rei;
+
+import com.tiviacz.travelersbackpack.inventory.screen.TravelersBackpackBaseScreenHandler;
+import me.shedaniel.rei.api.common.display.SimpleGridMenuDisplay;
+import me.shedaniel.rei.api.common.plugins.REIServerPlugin;
+import me.shedaniel.rei.api.common.transfer.RecipeFinder;
+import me.shedaniel.rei.api.common.transfer.info.MenuInfoContext;
+import me.shedaniel.rei.api.common.transfer.info.MenuInfoRegistry;
+import me.shedaniel.rei.api.common.transfer.info.simple.SimpleGridMenuInfo;
+import me.shedaniel.rei.api.common.transfer.info.simple.SimpleMenuInfoProvider;
+import me.shedaniel.rei.api.common.transfer.info.stack.SlotAccessor;
+import me.shedaniel.rei.plugin.common.BuiltinPlugin;
+import net.minecraft.item.ItemStack;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ReiCompat implements REIServerPlugin
+{
+    @Override
+    public double getPriority() {
+        return 0D;
+    }
+
+    @Override
+    public void registerMenuInfo(MenuInfoRegistry registry)
+    {
+        registry.register(BuiltinPlugin.CRAFTING, TravelersBackpackBaseScreenHandler.class, SimpleMenuInfoProvider.of(GridMenuInfo::new));
+    }
+
+    private static class GridMenuInfo<T extends TravelersBackpackBaseScreenHandler, D extends SimpleGridMenuDisplay> extends RecipeBookGridMenuInfo<T, D> {
+
+        public GridMenuInfo(D display) {
+            super(display);
+        }
+
+        @Override
+        public IntStream getInputStackSlotIds(MenuInfoContext<T, ?, D> context) {
+            return IntStream.range(1, 10);
+        }
+    }
+
+    public static class RecipeBookGridMenuInfo<T extends TravelersBackpackBaseScreenHandler, D extends SimpleGridMenuDisplay> implements SimpleGridMenuInfo<T, D> {
+        private final D display;
+
+        public RecipeBookGridMenuInfo(D display) {
+            this.display = display;
+        }
+
+        @Override
+        public int getCraftingResultSlotIndex(T menu) {
+            return 0;
+        }
+
+        @Override
+        public int getCraftingWidth(T menu) {
+            return 3;
+        }
+
+        @Override
+        public int getCraftingHeight(T menu) {
+            return 3;
+        }
+
+        @Override
+        public void clearInputSlots(T menu) {
+            menu.craftMatrix.clear();
+        }
+
+        @Override
+        public void populateRecipeFinder(T menu, RecipeFinder finder) {
+            menu.craftMatrix.provideRecipeInputs(new net.minecraft.recipe.RecipeMatcher() {
+                @Override
+                public void addUnenchantedInput(ItemStack stack) {
+                    finder.addNormalItem(stack);
+                }
+            });
+        }
+
+        @Override
+        public D getDisplay() {
+            return display;
+        }
+
+        @Override
+        public Iterable<SlotAccessor> getInventorySlots(MenuInfoContext<T, ?, D> context)
+        {
+            return IntStream.range(10, 91).filter(i -> i <= 48 || i >= 55).mapToObj(index -> SlotAccessor.fromSlot(context.getMenu().getSlot(index))).collect(Collectors.toList());
+        }
+    }
+}
