7dd48e8944a460a438bd1b670a6d959b636f3964
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2022-11-11 22:52:13 +0100

    🔧 Tweaked wrong ability cooldowns

---
+++ src/main/java/com/tiviacz/travelersbackpack/util/TimeUtils.java
@@ -0,0 +1,21 @@
+package com.tiviacz.travelersbackpack.util;
+
+import java.util.Random;
+
+public class TimeUtils
+{
+    public static int randomTime(Random rand, int maxSeconds)
+    {
+        return rand.nextInt(seconds(maxSeconds));
+    }
+
+    public static int randomTime(Random rand, int minSeconds, int maxSeconds)
+    {
+        return rand.nextInt((seconds(maxSeconds) - seconds(minSeconds)) + 1) + seconds(minSeconds);
+    }
+
+    public static int seconds(int seconds)
+    {
+        return seconds * 20;
+    }
+}
