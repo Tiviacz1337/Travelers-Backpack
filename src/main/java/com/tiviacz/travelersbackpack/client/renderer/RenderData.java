80e59d0dae528208377919eaa9d755b15988b1dc
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2022-12-04 20:08:36 +0100

    🔧 RenderData Helper

---
+++ src/main/java/com/tiviacz/travelersbackpack/client/renderer/RenderData.java
@@ -0,0 +1,99 @@
+package com.tiviacz.travelersbackpack.client.renderer;
+
+import com.tiviacz.travelersbackpack.config.TravelersBackpackConfig;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
+import net.fabricmc.fabric.api.transfer.v1.storage.base.SingleVariantStorage;
+import net.fabricmc.fabric.impl.transfer.fluid.FluidVariantImpl;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.DyeColor;
+
+public class RenderData
+{
+    private final ItemStack stack;
+    private final PlayerEntity player;
+    private final SingleVariantStorage<FluidVariant> leftTank = new SingleVariantStorage<FluidVariant>() {
+        @Override
+        protected FluidVariant getBlankVariant() {
+            return FluidVariant.blank();
+        }
+
+        @Override
+        protected long getCapacity(FluidVariant variant)
+        {
+            return TravelersBackpackConfig.tanksCapacity;
+        }
+    };
+
+    private final SingleVariantStorage<FluidVariant> rightTank = new SingleVariantStorage<FluidVariant>() {
+        @Override
+        protected FluidVariant getBlankVariant() {
+            return FluidVariant.blank();
+        }
+
+        @Override
+        protected long getCapacity(FluidVariant variant)
+        {
+            return TravelersBackpackConfig.tanksCapacity;
+        }
+    };
+
+    private final String LEFT_TANK = "LeftTank";
+    private final String LEFT_TANK_AMOUNT = "LeftTankAmount";
+    private final String RIGHT_TANK = "RightTank";
+    private final String RIGHT_TANK_AMOUNT = "RightTankAmount";
+    private final String COLOR = "Color";
+    private final String SLEEPING_BAG_COLOR = "SleepingBagColor";
+
+    public RenderData(PlayerEntity player, ItemStack stack, boolean loadData)
+    {
+        this.player = player;
+        this.stack = stack;
+
+        if(loadData)
+        {
+            this.loadDataFromStack(stack);
+        }
+    }
+
+    public SingleVariantStorage<FluidVariant> getLeftTank()
+    {
+        return this.leftTank;
+    }
+
+    public SingleVariantStorage<FluidVariant> getRightTank()
+    {
+        return this.rightTank;
+    }
+
+    public ItemStack getItemStack()
+    {
+        return this.stack;
+    }
+
+    public int getSleepingBagColor()
+    {
+        if(this.stack.getOrCreateNbt().contains(SLEEPING_BAG_COLOR))
+        {
+            return this.stack.getOrCreateNbt().getInt(SLEEPING_BAG_COLOR);
+        }
+        return DyeColor.RED.getId();
+    }
+
+    public void loadDataFromStack(ItemStack stack)
+    {
+        if(!stack.isEmpty() && stack.hasNbt())
+        {
+            loadTanks(stack.getOrCreateNbt());
+        }
+    }
+
+    public void loadTanks(NbtCompound compound)
+    {
+        this.leftTank.variant = FluidVariantImpl.fromNbt(compound.getCompound(LEFT_TANK));
+        this.rightTank.variant = FluidVariantImpl.fromNbt(compound.getCompound(RIGHT_TANK));
+        this.leftTank.amount = compound.getLong(LEFT_TANK_AMOUNT);
+        this.rightTank.amount = compound.getLong(RIGHT_TANK_AMOUNT);
+    }
+}
