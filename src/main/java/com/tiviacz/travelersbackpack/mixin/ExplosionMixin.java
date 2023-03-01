1819eb040973dcaffbaa673571b50ea93dabbde3
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2023-03-01 21:37:14 +0100

    🐛 Fixed disappearing backpack if player was killed by explosion

--- src/main/java/com/tiviacz/travelersbackpack/mixin/ExplosionMixin.java
+++ src/main/java/com/tiviacz/travelersbackpack/mixin/ExplosionMixin.java
@@ -1,6 +1,9 @@
 package com.tiviacz.travelersbackpack.mixin;
 
 import com.tiviacz.travelersbackpack.blocks.TravelersBackpackBlock;
+import com.tiviacz.travelersbackpack.items.TravelersBackpackItem;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.explosion.Explosion;
@@ -9,6 +12,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
@@ -24,5 +28,21 @@ public class ExplosionMixin
     public void collectBlocksAndDamageEntities(CallbackInfo ci)
     {
         this.affectedBlocks.removeIf(pos -> this.world.getBlockState(pos).getBlock() instanceof TravelersBackpackBlock);
+    }
+
+    @Redirect(
+            method = "collectBlocksAndDamageEntities",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/entity/Entity;isImmuneToExplosion()Z"
+            )
+    )
+    public boolean isImmuneToExplosion(Entity instance)
+    {
+        if(instance instanceof ItemEntity && ((ItemEntity) instance).getStack().getItem() instanceof TravelersBackpackItem)
+        {
+            return true;
+        }
+        return instance.isImmuneToExplosion();
     }
 }
