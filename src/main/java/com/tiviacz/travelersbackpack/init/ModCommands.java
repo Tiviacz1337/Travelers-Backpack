74e49220bd3fa8d57f95aa66f07307ba5485d697
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2023-03-15 23:21:06 +0100

    ✨ Added 'backpacks' backup folder to world folder

--- src/main/java/com/tiviacz/travelersbackpack/init/ModCommands.java
+++ src/main/java/com/tiviacz/travelersbackpack/init/ModCommands.java
@@ -1,6 +1,7 @@
 package com.tiviacz.travelersbackpack.init;
 
 import com.tiviacz.travelersbackpack.commands.AccessBackpackCommand;
+import com.tiviacz.travelersbackpack.commands.RestoreBackpackCommand;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 
 public class ModCommands
@@ -8,5 +9,6 @@ public class ModCommands
     public static void registerCommands()
     {
         CommandRegistrationCallback.EVENT.register(AccessBackpackCommand::register);
+        CommandRegistrationCallback.EVENT.register(RestoreBackpackCommand::register);
     }
 }
