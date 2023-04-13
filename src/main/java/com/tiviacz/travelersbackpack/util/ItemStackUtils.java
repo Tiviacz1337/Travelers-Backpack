071fe6eb43b79a4ca66ed0dc056f4f0670e45602
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2023-04-13 20:47:22 +0200

    🐛 Fixed memory slots not accepting damaged items

--- src/main/java/com/tiviacz/travelersbackpack/util/ItemStackUtils.java
+++ src/main/java/com/tiviacz/travelersbackpack/util/ItemStackUtils.java
@@ -1,6 +1,7 @@
 package com.tiviacz.travelersbackpack.util;
 
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraftforge.items.IItemHandler;
 
 public class ItemStackUtils
@@ -12,6 +13,34 @@ public class ItemStackUtils
 
     public static boolean isSameItemSameTags(ItemStack stack1, ItemStack stack2)
     {
-        return stack1.sameItem(stack2) && ItemStack.tagMatches(stack1, stack2);
+        return stack1.sameItemStackIgnoreDurability(stack2) && tagMatches(stack1, stack2);
+    }
+
+    public static boolean tagMatches(ItemStack stack1, ItemStack stack2)
+    {
+        if (stack1.isEmpty() && stack2.isEmpty()) {
+            return true;
+        } else if (!stack1.isEmpty() && !stack2.isEmpty()) {
+            if (stack1.getTag() == null && stack2.getTag() != null) {
+                return false;
+            } else {
+
+                CompoundNBT copy1 = stack1.getTag() == null ? null : stack1.getTag().copy();
+                CompoundNBT copy2 = stack2.getTag() == null ? null : stack2.getTag().copy();
+
+                if(copy1.contains("Damage"))
+                {
+                    copy1.remove("Damage");
+                }
+                if(copy2.contains("Damage"))
+                {
+                    copy2.remove("Damage");
+                }
+
+                return (stack1.getTag() == null || copy1.equals(copy2)) && stack1.areCapsCompatible(stack2);
+            }
+        } else {
+            return false;
+        }
     }
 }
