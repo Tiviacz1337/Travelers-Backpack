93af1ce617203d4ed09bd115ba7f19fea498f805
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2023-04-21 14:23:03 +0200

    🐛 Fixed crash with memory slots

--- src/main/java/com/tiviacz/travelersbackpack/util/ItemStackUtils.java
+++ src/main/java/com/tiviacz/travelersbackpack/util/ItemStackUtils.java
@@ -28,13 +28,20 @@ public class ItemStackUtils
                 CompoundNBT copy1 = stack1.getTag() == null ? null : stack1.getTag().copy();
                 CompoundNBT copy2 = stack2.getTag() == null ? null : stack2.getTag().copy();
 
-                if(copy1.contains("Damage"))
+                if(copy1 != null)
                 {
-                    copy1.remove("Damage");
+                    if(copy1.contains("Damage"))
+                    {
+                        copy1.remove("Damage");
+                    }
                 }
-                if(copy2.contains("Damage"))
+
+                if(copy2 != null)
                 {
-                    copy2.remove("Damage");
+                    if(copy2.contains("Damage"))
+                    {
+                        copy2.remove("Damage");
+                    }
                 }
 
                 return (stack1.getTag() == null || copy1.equals(copy2)) && stack1.areCapsCompatible(stack2);
