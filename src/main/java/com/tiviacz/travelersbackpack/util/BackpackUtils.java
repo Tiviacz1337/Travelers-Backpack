9069274989efa0b633cda5ba52369a21dd7a7d9a
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2023-05-14 00:54:08 +0200

    🐛 Fixed backpack not placing below y = 0

--- src/main/java/com/tiviacz/travelersbackpack/util/BackpackUtils.java
+++ src/main/java/com/tiviacz/travelersbackpack/util/BackpackUtils.java
@@ -78,9 +78,18 @@ public class BackpackUtils
 
         if(TravelersBackpackConfig.voidProtection)
         {
-            if(y <= 0)
+            if(y <= level.getMinBuildHeight())
             {
-                tempY = 1;
+                tempY = level.getMinBuildHeight() + 5;
+            }
+        }
+
+        for(int i = tempY; i < level.getHeight(); i++)
+        {
+            if(level.getBlockState(new BlockPos((int)x, i, (int)z)).isAir())
+            {
+                tempY = i;
+                break;
             }
         }
 
@@ -140,23 +149,18 @@ public class BackpackUtils
 
         if(TravelersBackpackConfig.voidProtection)
         {
-            if(y <= 0)
+            if(y <= level.getMinBuildHeight())
             {
-                y = 1;
+                y = level.getMinBuildHeight() + 5;
             }
         }
 
-        if(y <= level.getMinBuildHeight() || y >= level.getHeight())
+        for(int i = y; i < level.getHeight(); i++)
         {
-            for(int i = 1; i < level.getHeight(); i++)
+            if(level.getBlockState(new BlockPos(playerPos.getX(), i, playerPos.getZ())).isAir())
             {
-                BlockPos pos = new BlockPos(playerPos.getX(), i, playerPos.getZ());
-
-                if(level.getBlockState(pos).isAir() || level.getBlockState(pos).getBlock().getExplosionResistance() > -1)
-                {
-                    y = i;
-                    break;
-                }
+                y = i;
+                break;
             }
         }
 
@@ -206,9 +210,9 @@ public class BackpackUtils
 
             if(TravelersBackpackConfig.voidProtection)
             {
-                if(y <= 0)
+                if(y <= level.getMinBuildHeight())
                 {
-                    y = 1;
+                    y = level.getMinBuildHeight() + 5;
                 }
             }
 
