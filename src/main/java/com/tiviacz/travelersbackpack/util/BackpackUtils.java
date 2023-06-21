d180ffec2324424a46d1e61af2acd59683b94f19
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2023-06-22 00:32:47 +0200

    🔧 Backpack can now be placed in fluids when player dies

--- src/main/java/com/tiviacz/travelersbackpack/util/BackpackUtils.java
+++ src/main/java/com/tiviacz/travelersbackpack/util/BackpackUtils.java
@@ -14,7 +14,6 @@ import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.DamageSource;
-import net.minecraft.util.Direction;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TranslationTextComponent;
@@ -189,7 +188,6 @@ public class BackpackUtils
                     cap.setContents(ItemStack.EMPTY);
                 });
             }
-
             return true;
         }
         return false;
@@ -213,17 +211,17 @@ public class BackpackUtils
                 }
             }
 
-            BlockPos spawn = getNearestEmptyChunkCoordinatesSpiral(player, world, X, Z, new BlockPos(X, y + Y, Z), 12, true, 1, (byte) 0, false);
+            BlockPos spawn = getNearestEmptyChunkCoordinatesSpiral(player, world, X, Z, new BlockPos(X, y + Y, Z), 12, true, 1, (byte) 0);
 
             if(spawn != null)
             {
-                return placeBackpack(stack, player, world, spawn.getX(), spawn.getY(), spawn.getZ(), Direction.UP);
+                return placeBackpack(stack, player, world, spawn.getX(), spawn.getY(), spawn.getZ());
             }
         }
         return false;
     }
 
-    public static boolean placeBackpack(ItemStack stack, PlayerEntity player, World world, int x, int y, int z, Direction facing)
+    public static boolean placeBackpack(ItemStack stack, PlayerEntity player, World world, int x, int y, int z)
     {
         if(stack.getTag() == null)
         {
@@ -236,43 +234,35 @@ public class BackpackUtils
 
         BlockPos targetPos = new BlockPos(x, y, z);
 
-        if(world.getBlockState(targetPos).getMaterial().isReplaceable())
+        if(!world.setBlockAndUpdate(targetPos, block.defaultBlockState()))
         {
-            if(!world.getBlockState(targetPos).getMaterial().isSolid())
-            {
-                if(!world.setBlockAndUpdate(targetPos, block.defaultBlockState()))
-                {
-                    return false;
-                }
-
-                player.sendMessage(new TranslationTextComponent("information.travelersbackpack.backpack_coords", targetPos.getX(), targetPos.getY(), targetPos.getZ()), player.getUUID());
-                LogHelper.info("Your backpack has been placed at" + " X: " + targetPos.getX() + " Y: " + targetPos.getY() + " Z: " + targetPos.getZ());
-
-                world.playSound(player, x, y, z, block.defaultBlockState().getSoundType().getPlaceSound(), SoundCategory.BLOCKS, 0.5F, 1.0F);
-                ((TravelersBackpackTileEntity)world.getBlockEntity(targetPos)).loadAllData(stack.getTag());
-
-                if(stack.hasCustomHoverName())
-                {
-                    ((TravelersBackpackTileEntity) world.getBlockEntity(targetPos)).setCustomName(stack.getDisplayName());
-                }
-
-                if(CapabilityUtils.isWearingBackpack(player))
-                {
-                    CapabilityUtils.getCapability(player).ifPresent(cap ->
-                    {
-                        cap.setWearable(ItemStack.EMPTY);
-                        cap.setContents(ItemStack.EMPTY);
-                    });
-                }
-
-                return true;
-            }
+            return false;
         }
-        return false;
+
+        player.sendMessage(new TranslationTextComponent("information.travelersbackpack.backpack_coords", targetPos.getX(), targetPos.getY(), targetPos.getZ()), player.getUUID());
+        LogHelper.info("Your backpack has been placed at" + " X: " + targetPos.getX() + " Y: " + targetPos.getY() + " Z: " + targetPos.getZ());
+
+        world.playSound(player, x, y, z, block.defaultBlockState().getSoundType().getPlaceSound(), SoundCategory.BLOCKS, 0.5F, 1.0F);
+        ((TravelersBackpackTileEntity)world.getBlockEntity(targetPos)).loadAllData(stack.getTag());
+
+        if(stack.hasCustomHoverName())
+        {
+            ((TravelersBackpackTileEntity)world.getBlockEntity(targetPos)).setCustomName(stack.getDisplayName());
+        }
+
+        if(CapabilityUtils.isWearingBackpack(player))
+        {
+            CapabilityUtils.getCapability(player).ifPresent(cap ->
+            {
+                cap.setWearable(ItemStack.EMPTY);
+                cap.setContents(ItemStack.EMPTY);
+            });
+        }
+        return true;
     }
 
-    public static String getConvertedTime(int ticks) {
-
+    public static String getConvertedTime(int ticks)
+    {
         int i = ticks / 20;
         int minutes = i / 60;
         int seconds = i % 60;
@@ -303,10 +293,9 @@ public class BackpackUtils
      * @param except Wether to include the origin of the search as a valid block.
      * @param steps  Number of steps of the recursive recursiveness that recurses through the recursion. It is the first size of the spiral, should be one (1) always at the first call.
      * @param pass   Pass switch for the witchcraft I can't quite explain. Set to 0 always at the beggining.
-     * @param type   True = for player, False = for backpack
      * @return The coordinates of the block in the chunk of the world of the game of the server of the owner of the computer, where you can place something above it.
      */
-    public static BlockPos getNearestEmptyChunkCoordinatesSpiral(PlayerEntity player, World world, int origX, int origZ, BlockPos pos, int radius, boolean except, int steps, byte pass, boolean type)
+    public static BlockPos getNearestEmptyChunkCoordinatesSpiral(PlayerEntity player, World world, int origX, int origZ, BlockPos pos, int radius, boolean except, int steps, byte pass)
     {
         //Spiral search, because Mr Darkona is awesome :)
         //This is so the backpack tries to get placed near the death point first
@@ -328,7 +317,7 @@ public class BackpackUtils
             {
                 for(; i <= pos.getX() + steps; i++)
                 {
-                    BlockPos blockPos = type ? checkCoordsForPlayer(player, world, origX, origZ, pos, except) : checkCoordsForBackpack(player, world, origX, origZ, pos, except);
+                    BlockPos blockPos = checkCoordsForBackpack(player, world, origX, origZ, pos, except);
 
                     if(blockPos != null)
                     {
@@ -336,14 +325,14 @@ public class BackpackUtils
                     }
                 }
                 pass++;
-                return getNearestEmptyChunkCoordinatesSpiral(player, world, origX, origZ, new BlockPos(i, pos.getY(), j), radius, except, steps, pass, type);
+                return getNearestEmptyChunkCoordinatesSpiral(player, world, origX, origZ, new BlockPos(i, pos.getY(), j), radius, except, steps, pass);
             }
 
             if(pass == 1)
             {
                 for(; j >= pos.getZ() - steps; j--)
                 {
-                    BlockPos blockPos = type ? checkCoordsForPlayer(player, world, origX, origZ, pos, except) : checkCoordsForBackpack(player, world, origX, origZ, pos, except);
+                    BlockPos blockPos = checkCoordsForBackpack(player, world, origX, origZ, pos, except);
 
                     if(blockPos != null)
                     {
@@ -352,7 +341,7 @@ public class BackpackUtils
                 }
                 pass--;
                 steps++;
-                return getNearestEmptyChunkCoordinatesSpiral(player, world, origX, origZ, new BlockPos(i, pos.getY(), j), radius, except, steps, pass, type);
+                return getNearestEmptyChunkCoordinatesSpiral(player, world, origX, origZ, new BlockPos(i, pos.getY(), j), radius, except, steps, pass);
             }
         }
 
@@ -362,7 +351,7 @@ public class BackpackUtils
             {
                 for(; i >= pos.getX() - steps; i--)
                 {
-                    BlockPos blockPos = type ? checkCoordsForPlayer(player, world, origX, origZ, pos, except) : checkCoordsForBackpack(player, world, origX, origZ, pos, except);
+                    BlockPos blockPos = checkCoordsForBackpack(player, world, origX, origZ, pos, except);
 
                     if(blockPos != null)
                     {
@@ -370,14 +359,14 @@ public class BackpackUtils
                     }
                 }
                 pass++;
-                return getNearestEmptyChunkCoordinatesSpiral(player, world, origX, origZ, new BlockPos(i, pos.getY(), j), radius, except, steps, pass, type);
+                return getNearestEmptyChunkCoordinatesSpiral(player, world, origX, origZ, new BlockPos(i, pos.getY(), j), radius, except, steps, pass);
             }
 
             if(pass == 1)
             {
                 for(; j <= pos.getZ() + steps; j++)
                 {
-                    BlockPos blockPos = type ? checkCoordsForPlayer(player, world, origX, origZ, pos, except) : checkCoordsForBackpack(player, world, origX, origZ, pos, except);
+                    BlockPos blockPos = checkCoordsForBackpack(player, world, origX, origZ, pos, except);
 
                     if(blockPos != null)
                     {
@@ -386,7 +375,7 @@ public class BackpackUtils
                 }
                 pass--;
                 steps++;
-                return getNearestEmptyChunkCoordinatesSpiral(player, world, origX, origZ, new BlockPos(i, pos.getY(), j), radius, except, steps, pass, type);
+                return getNearestEmptyChunkCoordinatesSpiral(player, world, origX, origZ, new BlockPos(i, pos.getY(), j), radius, except, steps, pass);
             }
         }
         return null;
@@ -394,24 +383,11 @@ public class BackpackUtils
 
     private static BlockPos checkCoordsForBackpack(PlayerEntity player, World world, int origX, int origZ, BlockPos pos, boolean except)
     {
-        if(except && world.loadedAndEntityCanStandOn(pos.below(), player) && world.isEmptyBlock(pos) && !areCoordinatesTheSame(new BlockPos(origX, pos.getY(), origZ), pos))
+        if(except && world.loadedAndEntityCanStandOn(pos.below(), player) && (world.isEmptyBlock(pos) || world.getBlockState(pos).getMaterial().isReplaceable()) && !areCoordinatesTheSame(new BlockPos(origX, pos.getY(), origZ), pos))
         {
             return pos;
         }
-        if(!except && world.loadedAndEntityCanStandOn(pos.below(), player) && world.isEmptyBlock(pos))
-        {
-            return pos;
-        }
-        return null;
-    }
-
-    private static BlockPos checkCoordsForPlayer(PlayerEntity player, World world, int origX, int origZ, BlockPos pos, boolean except)
-    {
-        if(except && world.loadedAndEntityCanStandOn(pos.below(), player) && world.isEmptyBlock(pos) && world.isEmptyBlock(pos.above()) && !areCoordinatesTheSame2D(origX, origZ, pos.getX(), pos.getZ()))
-        {
-            return pos;
-        }
-        if(!except && world.loadedAndEntityCanStandOn(pos.below(), player) && world.isEmptyBlock(pos) && world.isEmptyBlock(pos.above()))
+        if(!except && world.loadedAndEntityCanStandOn(pos.below(), player) && (world.isEmptyBlock(pos)|| world.getBlockState(pos).getMaterial().isReplaceable()))
         {
             return pos;
         }
@@ -421,10 +397,5 @@ public class BackpackUtils
     private static boolean areCoordinatesTheSame(BlockPos pos1, BlockPos pos2)
     {
         return pos1 == pos2;
-    }
-
-    private static boolean areCoordinatesTheSame2D(int X1, int Z1, int X2, int Z2)
-    {
-        return (X1 == X2 &&  Z1 == Z2);
     }
 }
