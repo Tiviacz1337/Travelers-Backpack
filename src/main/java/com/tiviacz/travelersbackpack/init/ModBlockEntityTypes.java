fe84719669931da05993ac47c1184d09788a9d4b
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2023-10-24 01:56:30 +0200

    🐛 Fixed crash

--- src/main/java/com/tiviacz/travelersbackpack/init/ModBlockEntityTypes.java
+++ src/main/java/com/tiviacz/travelersbackpack/init/ModBlockEntityTypes.java
@@ -80,6 +80,11 @@ public class ModBlockEntityTypes
     {
         Direction backpackDirection = blockEntity.getBlockDirection(blockEntity);
 
+        if(clickedDirection == null)
+        {
+            return blockEntity.rightTank;
+        }
+
         return switch (clickedDirection) {
             case NORTH ->
                     backpackDirection == Direction.WEST ? blockEntity.leftTank : backpackDirection == Direction.EAST ? blockEntity.rightTank : null;
@@ -99,6 +104,11 @@ public class ModBlockEntityTypes
 
         Storage<ItemVariant> mainInv = InventoryStorageImpl.of(blockEntity.inventory, null);
         Storage<ItemVariant> craftingInv = InventoryStorageImpl.of(blockEntity.craftingInventory, null);
+
+        if(clickedDirection == null)
+        {
+            return mainInv;
+        }
 
         switch(clickedDirection)
         {
