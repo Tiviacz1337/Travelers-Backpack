86ad5edf6fbe405051fcd65567920f4c017ed460
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2023-11-03 01:35:10 +0100

    ✨ Sleeping bag can be used without the backpack

--- src/main/java/com/tiviacz/travelersbackpack/items/SleepingBagItem.java
+++ src/main/java/com/tiviacz/travelersbackpack/items/SleepingBagItem.java
@@ -1,20 +1,12 @@
 package com.tiviacz.travelersbackpack.items;
 
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.item.BedItem;
 import net.minecraft.world.level.block.Block;
 
-public class SleepingBagItem extends BlockItem
+public class SleepingBagItem extends BedItem
 {
     public SleepingBagItem(Block block, Properties properties)
     {
         super(block, properties);
-    }
-
-    @Override
-    public InteractionResult useOn(UseOnContext context)
-    {
-        return InteractionResult.FAIL;
     }
 }
