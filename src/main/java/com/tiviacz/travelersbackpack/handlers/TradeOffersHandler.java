81b7958cdf736e63b37543106052c171563ae58d
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2023-12-28 18:25:56 +0100

    🔧 Villager backpack can be obtained from librarian villager on 3rd level

--- src/main/java/com/tiviacz/travelersbackpack/handlers/TradeOffersHandler.java
+++ src/main/java/com/tiviacz/travelersbackpack/handlers/TradeOffersHandler.java
@@ -2,14 +2,10 @@ package com.tiviacz.travelersbackpack.handlers;
 
 import com.tiviacz.travelersbackpack.config.TravelersBackpackConfig;
 import com.tiviacz.travelersbackpack.init.ModItems;
-import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.annotation.Nullable;
 import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
-import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.util.math.random.Random;
 import net.minecraft.village.TradeOffer;
-import net.minecraft.village.TradeOffers;
 import net.minecraft.village.VillagerProfession;
 
 public class TradeOffersHandler
@@ -18,17 +14,9 @@ public class TradeOffersHandler
     {
         if(TravelersBackpackConfig.enableVillagerTrade)
         {
-            TradeOfferHelper.registerVillagerOffers(VillagerProfession.LIBRARIAN, 5, factories -> factories.add(new BackpackVillagerTrade()));
-        }
-    }
-
-    private static class BackpackVillagerTrade implements TradeOffers.Factory
-    {
-        @Nullable
-        @Override
-        public TradeOffer create(Entity entity, Random random)
-        {
-            return new TradeOffer(new ItemStack(Items.EMERALD, random.nextInt(64) + 48), new ItemStack(ModItems.VILLAGER_TRAVELERS_BACKPACK, 1), 1, 5, 0.5F);
+            TradeOfferHelper.registerVillagerOffers(VillagerProfession.LIBRARIAN, 3, factories -> factories.add(
+                    (trader, random) -> new TradeOffer(new ItemStack(Items.EMERALD, random.nextInt(64) + 48),
+                            new ItemStack(ModItems.VILLAGER_TRAVELERS_BACKPACK.asItem(), 1), 1, 50, 0.5F)));
         }
     }
 }
