10ae59c907cd5708cab09bd1514824fc7485157a
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2024-04-08 20:01:58 +0200

    🐛 Fixed bug with disappearing items

--- src/main/java/com/tiviacz/travelersbackpack/inventory/TravelersBackpackInventory.java
+++ src/main/java/com/tiviacz/travelersbackpack/inventory/TravelersBackpackInventory.java
@@ -150,6 +150,10 @@ public class TravelersBackpackInventory implements ITravelersBackpackInventory
     {
         compound.put(LEFT_TANK, this.leftTank.writeToNbt(new NbtCompound()));
         compound.put(RIGHT_TANK, this.rightTank.writeToNbt(new NbtCompound()));
+
+        //#TODO clean up in 1.21, old data converter
+        if(compound.contains(LEFT_TANK_AMOUNT)) compound.remove(LEFT_TANK_AMOUNT);
+        if(compound.contains(RIGHT_TANK_AMOUNT)) compound.remove(RIGHT_TANK_AMOUNT);
     }
 
     @Override
