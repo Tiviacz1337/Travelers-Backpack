c5e86fef7305fc0789a793e977af7f7d14b3f339
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2024-05-18 02:37:11 +0200

    ✨ Added JEI compat

---
+++ src/main/java/com/tiviacz/travelersbackpack/compat/jei/BlockEntityTransferInfo.java
@@ -0,0 +1,77 @@
+package com.tiviacz.travelersbackpack.compat.jei;
+
+import com.tiviacz.travelersbackpack.init.ModScreenHandlerTypes;
+import com.tiviacz.travelersbackpack.inventory.screen.TravelersBackpackBlockEntityScreenHandler;
+import mezz.jei.api.constants.RecipeTypes;
+import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.recipe.transfer.IRecipeTransferInfo;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.recipe.CraftingRecipe;
+import net.minecraft.recipe.RecipeEntry;
+import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.screen.slot.Slot;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class BlockEntityTransferInfo implements IRecipeTransferInfo<TravelersBackpackBlockEntityScreenHandler, RecipeEntry<CraftingRecipe>>
+{
+    @Override
+    public Class<? extends TravelersBackpackBlockEntityScreenHandler> getContainerClass()
+    {
+        return TravelersBackpackBlockEntityScreenHandler.class;
+    }
+
+    @Override
+    public Optional<ScreenHandlerType<TravelersBackpackBlockEntityScreenHandler>> getMenuType()
+    {
+        return Optional.of(ModScreenHandlerTypes.TRAVELERS_BACKPACK_BLOCK_ENTITY);
+    }
+
+    @Override
+    public RecipeType<RecipeEntry<CraftingRecipe>> getRecipeType()
+    {
+        return RecipeTypes.CRAFTING;
+    }
+
+    @Override
+    public boolean canHandle(TravelersBackpackBlockEntityScreenHandler container, RecipeEntry<CraftingRecipe> recipe)
+    {
+        return container.inventory.getSettingsManager().hasCraftingGrid();
+    }
+
+    @Override
+    public List<Slot> getRecipeSlots(TravelersBackpackBlockEntityScreenHandler container, RecipeEntry<CraftingRecipe> recipe)
+    {
+        List<Slot> list = new ArrayList<>();
+        int firstCraftSlot = container.inventory.getCombinedInventory().size() - 8;
+
+        for(int i = 0; i < 9; i++)
+        {
+            list.add(container.getSlot(firstCraftSlot + i));
+        }
+
+        return list;
+    }
+
+    @Override
+    public List<Slot> getInventorySlots(TravelersBackpackBlockEntityScreenHandler container, RecipeEntry<CraftingRecipe> recipe)
+    {
+        List<Slot> list = new ArrayList<>();
+
+        //Backpack Inv
+        for(int i = 1; i <= container.inventory.getInventory().size(); i++)
+        {
+            list.add(container.getSlot(i));
+        }
+
+        //Player Inv
+        for(int i = container.inventory.getCombinedInventory().size(); i < container.inventory.getCombinedInventory().size() + PlayerInventory.MAIN_SIZE; i++)
+        {
+            list.add(container.getSlot(i));
+        }
+
+        return list;
+    }
+}
---
+++ src/main/java/com/tiviacz/travelersbackpack/compat/jei/ItemTransferHandler.java
@@ -0,0 +1,43 @@
+package com.tiviacz.travelersbackpack.compat.jei;
+
+import com.tiviacz.travelersbackpack.init.ModNetwork;
+import com.tiviacz.travelersbackpack.inventory.SettingsManager;
+import com.tiviacz.travelersbackpack.inventory.screen.TravelersBackpackItemScreenHandler;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.helpers.IStackHelper;
+import mezz.jei.api.recipe.transfer.IRecipeTransferError;
+import mezz.jei.api.recipe.transfer.IRecipeTransferHandlerHelper;
+import mezz.jei.api.recipe.transfer.IRecipeTransferInfo;
+import mezz.jei.common.network.IConnectionToServer;
+import mezz.jei.library.transfer.BasicRecipeTransferHandler;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.recipe.CraftingRecipe;
+import net.minecraft.recipe.RecipeEntry;
+import org.jetbrains.annotations.Nullable;
+
+public class ItemTransferHandler extends BasicRecipeTransferHandler<TravelersBackpackItemScreenHandler, RecipeEntry<CraftingRecipe>>
+{
+    public ItemTransferHandler(IConnectionToServer serverConnection, IStackHelper stackHelper, IRecipeTransferHandlerHelper handlerHelper, IRecipeTransferInfo<TravelersBackpackItemScreenHandler, RecipeEntry<CraftingRecipe>> transferInfo)
+    {
+        super(serverConnection, stackHelper, handlerHelper, transferInfo);
+    }
+
+    @Nullable
+    @Override
+    public IRecipeTransferError transferRecipe(TravelersBackpackItemScreenHandler container, RecipeEntry<CraftingRecipe> recipe, IRecipeSlotsView recipeSlotsView, PlayerEntity player, boolean maxTransfer, boolean doTransfer)
+    {
+        if(doTransfer)
+        {
+            container.inventory.getSettingsManager().set(SettingsManager.CRAFTING, SettingsManager.SHOW_CRAFTING_GRID, (byte)1);
+
+            PacketByteBuf buf = PacketByteBufs.create();
+            buf.writeByte(container.inventory.getScreenID()).writeByte(SettingsManager.CRAFTING).writeInt(SettingsManager.SHOW_CRAFTING_GRID).writeByte((byte)1);
+
+            ClientPlayNetworking.send(ModNetwork.SETTINGS_ID, buf);
+        }
+        return super.transferRecipe(container, recipe, recipeSlotsView, player, maxTransfer, doTransfer);
+    }
+}
