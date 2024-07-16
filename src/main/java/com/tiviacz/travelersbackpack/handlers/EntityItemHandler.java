c752e2b111478e7fde8cd9dd9a3d13dc0df1fc7e
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2024-07-16 12:31:53 +0200

    🔧 Backpack item entity never despawns

--- src/main/java/com/tiviacz/travelersbackpack/handlers/EntityItemHandler.java
+++ src/main/java/com/tiviacz/travelersbackpack/handlers/EntityItemHandler.java
@@ -17,7 +17,7 @@ public class EntityItemHandler
 
             if(itemEntity.getStack().getItem() instanceof TravelersBackpackItem)
             {
-                itemEntity.setCovetedItem();
+                itemEntity.setNeverDespawn();
                 entity.setInvulnerable(true);
             }
         });
