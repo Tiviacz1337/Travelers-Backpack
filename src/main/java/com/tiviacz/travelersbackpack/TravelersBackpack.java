8a2b1a3a24dd1cb8176d640e7821d6f10fc942e7
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2024-09-28 15:36:04 +0200

    ✨ Added Tough as Nails compatibility

--- src/main/java/com/tiviacz/travelersbackpack/TravelersBackpack.java
+++ src/main/java/com/tiviacz/travelersbackpack/TravelersBackpack.java
@@ -43,6 +43,7 @@ public class TravelersBackpack
     public static boolean corpseLoaded;
     public static boolean gravestoneLoaded;
 
+    public static boolean toughasnailsLoaded;
     public static boolean comfortsLoaded;
     public static boolean endermanOverhaulLoaded;
 
@@ -80,6 +81,7 @@ public class TravelersBackpack
         corpseLoaded = ModList.get().isLoaded("corpse");
         gravestoneLoaded = ModList.get().isLoaded("gravestone");
 
+        toughasnailsLoaded = ModList.get().isLoaded("toughasnails");
         comfortsLoaded = ModList.get().isLoaded("comforts");
         endermanOverhaulLoaded = ModList.get().isLoaded("endermanoverhaul");
     }
---
+++ src/main/java/com/tiviacz/travelersbackpack/compat/toughasnails/ToughAsNailsPotionEffect.java
@@ -0,0 +1,52 @@
+package com.tiviacz.travelersbackpack.compat.toughasnails;
+
+import com.tiviacz.travelersbackpack.api.fluids.EffectFluid;
+import com.tiviacz.travelersbackpack.init.ModFluids;
+import com.tiviacz.travelersbackpack.util.Reference;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.fluids.FluidStack;
+import toughasnails.api.potion.TANEffects;
+import toughasnails.api.thirst.IThirst;
+import toughasnails.api.thirst.ThirstHelper;
+import toughasnails.config.ThirstConfig;
+import toughasnails.init.ModConfig;
+
+public class ToughAsNailsPotionEffect extends EffectFluid
+{
+    public ToughAsNailsPotionEffect()
+    {
+        super("toughasnails:potion", ModFluids.POTION_FLUID.get(), Reference.POTION);
+    }
+
+    @Override
+    public void affectDrinker(FluidStack fluidStack, Level level, Entity entity)
+    {
+        if(entity instanceof Player player)
+        {
+            if(!level.isClientSide)
+            {
+                //Data for potion
+                IThirst thirst = ThirstHelper.getThirst(player);
+                int drink_thirst = 3;
+                float drink_hydration = 0.4F;
+                float drink_poison_chance = 0.25F;
+                //thirst.drink(drink_thirst, drink_hydration);
+                thirst.addThirst(drink_thirst);
+                thirst.addHydration(drink_hydration);
+
+                if(level.random.nextFloat() < drink_poison_chance) {
+                    player.addEffect(new MobEffectInstance(TANEffects.THIRST.get(), 600));
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean canExecuteEffect(FluidStack stack, Level level, Entity entity)
+    {
+        return stack.getAmount() >= amountRequired;
+    }
+}
