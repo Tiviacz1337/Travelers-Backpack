5ba10995918763af5ef00d2600204e63432d4e7d
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2025-01-09 02:22:21 +0100

    ✨ Added Polymorph compatibility

--- src/main/java/com/tiviacz/travelersbackpack/TravelersBackpack.java
+++ src/main/java/com/tiviacz/travelersbackpack/TravelersBackpack.java
@@ -1,6 +1,7 @@
 package com.tiviacz.travelersbackpack;
 
 import com.tiviacz.travelersbackpack.blocks.TravelersBackpackBlock;
+import com.tiviacz.travelersbackpack.compat.polymorph.PolymorphCompat;
 import com.tiviacz.travelersbackpack.config.TravelersBackpackConfig;
 import com.tiviacz.travelersbackpack.fluids.EffectFluidRegistry;
 import com.tiviacz.travelersbackpack.handlers.ModClientEventHandler;
@@ -38,6 +39,7 @@ public class TravelersBackpack {
     public static boolean endermanOverhaulLoaded;
 
     public static boolean jeiLoaded;
+    public static boolean polymorphLoaded;
 
     public TravelersBackpack() {
         ForgeMod.enableMilkFluid();
@@ -77,6 +79,7 @@ public class TravelersBackpack {
         endermanOverhaulLoaded = ModList.get().isLoaded("endermanoverhaul");
 
         jeiLoaded = ModList.get().isLoaded("jei");
+        polymorphLoaded = ModList.get().isLoaded("polymorph");
     }
 
     private void setup(final FMLCommonSetupEvent event) {
@@ -96,6 +99,7 @@ public class TravelersBackpack {
             ModClientEventHandler.registerBlockEntityRenderers();
             ModClientEventHandler.registerItemModelProperties();
         });
+        if(polymorphLoaded) PolymorphCompat.registerWidget();
         // if(accessoriesLoaded) TravelersBackpackAccessory.initClient();
         // if(curiosLoaded && !accessoriesLoaded) TravelersBackpackCurio.registerCurioRenderer();
     }
