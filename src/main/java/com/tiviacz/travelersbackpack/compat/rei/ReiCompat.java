6c22c8dc1a7154f9caed7a5e6d2a8a030ff866fa
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2025-01-27 02:57:14 +0100

    🐛 Fixed REI randomly opening

--- src/main/java/com/tiviacz/travelersbackpack/compat/rei/ReiCompat.java
+++ src/main/java/com/tiviacz/travelersbackpack/compat/rei/ReiCompat.java
@@ -78,7 +78,7 @@ public class ReiCompat implements REIClientPlugin {
         public Result handle(Context context) {
             if(context.getMenu() instanceof BackpackBaseMenu menu) {
                 CraftingUpgrade upgrade = menu.getWrapper().getUpgradeManager().craftingUpgrade.get();
-                if(!upgrade.isTabOpened()) {
+                if(!upgrade.isTabOpened() && context.isActuallyCrafting()) {
                     PacketDistributor.sendToServer(new ServerboundTabPacket(upgrade.getDataHolderSlot(), true, ServerboundTabPacket.TAB_OPEN));
                 }
             }
