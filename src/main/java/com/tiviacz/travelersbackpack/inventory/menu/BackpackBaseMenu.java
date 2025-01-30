9716ee6a9cf205696f8bf6fdc159c1b8a3bd1c2a
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2025-01-30 13:38:14 +0100

    🐛 Fixed duplication glitch with Filter Slots

--- src/main/java/com/tiviacz/travelersbackpack/inventory/menu/BackpackBaseMenu.java
+++ src/main/java/com/tiviacz/travelersbackpack/inventory/menu/BackpackBaseMenu.java
@@ -20,10 +20,7 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.Container;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.inventory.CraftingContainer;
-import net.minecraft.world.inventory.MenuType;
-import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.inventory.*;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeType;
@@ -36,13 +33,12 @@ import java.util.List;
 import java.util.Optional;
 
 public class BackpackBaseMenu extends AbstractContainerMenu {
+    public final Player player;
     protected final Inventory inventory;
     protected final BackpackWrapper wrapper;
-
     public List<UpgradeSlotItemHandler> upgradeSlot = new ArrayList<>();
     public int extendedScreenOffset = 0;
     public int unmodifiableSlotCount = 0;
-
     public int BACKPACK_INV_START = 0, BACKPACK_INV_END;
     public int TOOL_START, TOOL_END;
     public int BUCKET_LEFT_IN, BUCKET_LEFT_OUT;
@@ -50,8 +46,6 @@ public class BackpackBaseMenu extends AbstractContainerMenu {
     public int PLAYER_INV_START, PLAYER_HOT_END;
     public int CRAFTING_RESULT;
     public int CRAFTING_GRID_START, CRAFTING_GRID_END;
-
-    public final Player player;
 
     public BackpackBaseMenu(final MenuType<?> type, final int windowID, final Inventory inventory, final BackpackWrapper wrapper) {
         super(type, windowID);
@@ -261,6 +255,21 @@ public class BackpackBaseMenu extends AbstractContainerMenu {
                     this.broadcastChanges();
                 }
             });
+        }
+    }
+
+    @Override
+    protected void doClick(int pSlotId, int pButton, ClickType pClickType, Player pPlayer) {
+        if(pSlotId != -999 && this.slots.get(pSlotId) instanceof FilterSlotItemHandler filterSlot) {
+            if(getCarried().isEmpty() && pClickType == ClickType.PICKUP) { //Remove item from filter slot
+                super.doClick(pSlotId, pButton, pClickType, pPlayer);
+            } else if(!getCarried().isEmpty()) { //Add item to filter slot
+                if(!filterSlot.hasItem()) {
+                    filterSlot.set(getCarried().copyWithCount(1));
+                }
+            }
+        } else {
+            super.doClick(pSlotId, pButton, pClickType, pPlayer);
         }
     }
 
