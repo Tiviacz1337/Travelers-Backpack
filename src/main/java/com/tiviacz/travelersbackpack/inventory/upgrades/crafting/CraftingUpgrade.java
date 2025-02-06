84bb9d54a4bcbdcf6dd1afa2cf9776edc72cf37e
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2025-02-06 15:31:38 +0100

    🐛 Fixed JEI item transfer issue

--- src/main/java/com/tiviacz/travelersbackpack/inventory/upgrades/crafting/CraftingUpgrade.java
+++ src/main/java/com/tiviacz/travelersbackpack/inventory/upgrades/crafting/CraftingUpgrade.java
@@ -75,7 +75,12 @@ public class CraftingUpgrade extends UpgradeBase {
 
                     @Override
                     public boolean mayPlace(ItemStack pStack) {
-                        return isTabOpened();
+                        return true;
+                    }
+
+                    @Override
+                    public boolean mayPickup(Player pPlayer) {
+                        return true;
                     }
                 });
             }
