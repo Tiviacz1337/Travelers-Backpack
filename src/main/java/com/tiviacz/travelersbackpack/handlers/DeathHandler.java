95851c796b2198bfaa86e069a8a38ebbe1f4b804
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2025-03-02 00:19:44 +0100

    🐛 Fix occasional crash

--- src/main/java/com/tiviacz/travelersbackpack/handlers/DeathHandler.java
+++ src/main/java/com/tiviacz/travelersbackpack/handlers/DeathHandler.java
@@ -5,6 +5,7 @@ import com.tiviacz.travelersbackpack.common.BackpackAbilities;
 import com.tiviacz.travelersbackpack.common.BackpackManager;
 import com.tiviacz.travelersbackpack.component.ComponentUtils;
 import com.tiviacz.travelersbackpack.config.TravelersBackpackConfig;
+import com.tiviacz.travelersbackpack.init.ModItems;
 import com.tiviacz.travelersbackpack.item.TravelersBackpackItem;
 import com.tiviacz.travelersbackpack.network.ClientboundSendMessagePacket;
 import com.tiviacz.travelersbackpack.util.BackpackDeathHelper;
@@ -24,8 +25,10 @@ public class DeathHandler {
         ServerLivingEntityEvents.ALLOW_DEATH.register((livingEntity, damageSource, damageAmount) -> {
             if(livingEntity instanceof ServerPlayer player) {
                 if(ComponentUtils.isWearingBackpack(player)) {
-                    if(TravelersBackpackConfig.getConfig().backpackAbilities.enableBackpackAbilities && BackpackAbilities.creeperAbility(player)) {
-                        return false;
+                    if(BackpackAbilities.ABILITIES.checkBackpack(player, ModItems.CREEPER_TRAVELERS_BACKPACK)) {
+                        if(TravelersBackpackConfig.getConfig().backpackAbilities.enableBackpackAbilities && BackpackAbilities.creeperAbility(player)) {
+                            return false;
+                        }
                     }
                 }
             }
