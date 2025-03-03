817a98605341dafae234a71cc60bd68cdeb3a1d8
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2025-03-03 13:32:41 +0100

    🔧 Backpack abilities no longer display effect icons

--- src/main/java/com/tiviacz/travelersbackpack/common/BackpackAbilities.java
+++ src/main/java/com/tiviacz/travelersbackpack/common/BackpackAbilities.java
@@ -114,7 +114,7 @@ public class BackpackAbilities {
                 if(!effectHasCooldown) {
                     Collection<BackpackEffect> backpackEffects = getBackpackEffects().get(backpackItem);
                     for(BackpackEffect backpackEffect : backpackEffects) {
-                        addTimedMobEffect(player, backpackEffect.effect(), backpackEffect.minDuration(), backpackEffect.maxDuration(), backpackEffect.amplifier(), false, false, true);
+                        addTimedMobEffect(player, backpackEffect.effect(), backpackEffect.minDuration(), backpackEffect.maxDuration(), backpackEffect.amplifier(), false, false, false);
                     }
                 } else { //If yes, then check if there's active cooldown
                     //If no active cooldown
@@ -133,7 +133,7 @@ public class BackpackAbilities {
                             //    magmaCubeAbility(backpack, player, backpackEffect.minDuration(), backpackEffect.maxDuration(), backpackEffect.amplifier());
                             //}
 
-                            addTimedMobEffect(player, backpackEffect.effect(), backpackEffect.minDuration(), backpackEffect.maxDuration(), backpackEffect.amplifier(), false, false, true);
+                            addTimedMobEffect(player, backpackEffect.effect(), backpackEffect.minDuration(), backpackEffect.maxDuration(), backpackEffect.amplifier(), false, false, false);
                         }
                         //Apply cooldown
                         setCooldown(CapabilityUtils.getBackpackWrapper(player, backpack), backpackItem);
@@ -656,7 +656,7 @@ public class BackpackAbilities {
 
     public void ocelotAbility(Player player) {
         if(player.level().getNearestEntity(Monster.class, ocelotAbilityTargeting, player, player.getX(), player.getY(), player.getZ(), player.getBoundingBox().inflate(6.0D, 2.0D, 6.0D)) != null) {
-            addTimedMobEffect(player, MobEffects.MOVEMENT_SPEED, 20, 30, 0, false, false, true);
+            addTimedMobEffect(player, MobEffects.MOVEMENT_SPEED, 20, 30, 0, false, false, false);
         }
     }
 
