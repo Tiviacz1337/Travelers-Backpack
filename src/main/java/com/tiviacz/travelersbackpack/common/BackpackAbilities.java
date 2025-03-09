93e5a5fcb69ded5ff558a0dc77af6590bf1bd17e
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2025-03-09 01:01:45 +0100

    🔧 Wither backpack ability buff

--- src/main/java/com/tiviacz/travelersbackpack/common/BackpackAbilities.java
+++ src/main/java/com/tiviacz/travelersbackpack/common/BackpackAbilities.java
@@ -206,6 +206,11 @@ public class BackpackAbilities {
                 cowAbility(backpack, player);
                 return true;
             }
+
+            if(backpackItem == ModItems.WITHER_TRAVELERS_BACKPACK.get()) {
+                witherAbilityTick(player);
+                return false;
+            }
         }
         return tickCooldown;
     }
@@ -553,6 +558,14 @@ public class BackpackAbilities {
                         player.getY() + 0.1D,
                         player.getZ() + (level.random.nextDouble() - 0.5D) * (double)player.getDimensions(Pose.STANDING).width(),
                         0.0D, 1.5D, 0.0D);
+            }
+        }
+    }
+
+    public static void witherAbilityTick(Player player) {
+        if(ABILITIES.checkBackpack(player, ModItems.WITHER_TRAVELERS_BACKPACK.get())) {
+            if(player.hasEffect(MobEffects.WITHER)) {
+                player.removeEffect(MobEffects.WITHER);
             }
         }
     }
