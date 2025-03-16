5f8abdc2497d2f7cb6f159f0d326ac74864d563c
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2025-03-16 20:48:14 +0100

    🔧 Changed magnet upgrade description

--- src/main/java/com/tiviacz/travelersbackpack/items/upgrades/MagnetUpgradeItem.java
+++ src/main/java/com/tiviacz/travelersbackpack/items/upgrades/MagnetUpgradeItem.java
@@ -1,11 +1,20 @@
 package com.tiviacz.travelersbackpack.items.upgrades;
 
 import com.tiviacz.travelersbackpack.config.TravelersBackpackConfig;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
+
+import java.util.List;
 
 public class MagnetUpgradeItem extends UpgradeItem {
     public MagnetUpgradeItem(Properties pProperties) {
-        super(pProperties, "magnet_upgrade");
+        super(pProperties, null);
     }
 
     @Override
@@ -14,5 +23,12 @@ public class MagnetUpgradeItem extends UpgradeItem {
             return TravelersBackpackConfig.SERVER.backpackUpgrades.magnetUpgradeSettings.enableMagnetUpgrade.get() && super.isEnabled(enabledFeatures);
         }
         return super.isEnabled(enabledFeatures); //return TravelersBackpackConfig.SERVER.backpackUpgrades.magnetUpgradeSettings.enableMagnetUpgrade.get() && super.isEnabled(enabledFeatures);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        tooltipComponents.add(Component.translatable("item.travelersbackpack.magnet_upgrade_tooltip", TravelersBackpackConfig.SERVER.backpackUpgrades.magnetUpgradeSettings.pullRange.get()).withStyle(ChatFormatting.BLUE));
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
     }
 }
