52bc75c83b4aa32323620b525364323295b0eac1
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2025-03-17 16:42:19 +0100

    🔧 Changed magnet upgrade description

--- src/main/java/com/tiviacz/travelersbackpack/item/upgrades/MagnetUpgradeItem.java
+++ src/main/java/com/tiviacz/travelersbackpack/item/upgrades/MagnetUpgradeItem.java
@@ -1,15 +1,31 @@
 package com.tiviacz.travelersbackpack.item.upgrades;
 
 import com.tiviacz.travelersbackpack.config.TravelersBackpackConfig;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+
+import java.util.List;
 
 public class MagnetUpgradeItem extends UpgradeItem {
     public MagnetUpgradeItem(Properties pProperties) {
-        super(pProperties, "magnet_upgrade");
+        super(pProperties, null);
     }
 
     @Override
     public boolean isEnabled(FeatureFlagSet enabledFeatures) {
         return TravelersBackpackConfig.getConfig().backpackUpgrades.magnetUpgradeSettings.enableUpgrade && super.isEnabled(enabledFeatures);
+    }
+
+    @Environment(EnvType.CLIENT)
+    @Override
+    public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        tooltipComponents.add(Component.translatable("item.travelersbackpack.magnet_upgrade_tooltip", TravelersBackpackConfig.getConfig().backpackUpgrades.magnetUpgradeSettings.pullRange).withStyle(ChatFormatting.BLUE));
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
     }
 }
