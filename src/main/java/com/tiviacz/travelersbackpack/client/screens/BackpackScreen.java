ce005687595bca44279aa1a646fb97627646cd8a
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2025-03-18 21:35:39 +0100

    🔧 Misc

--- src/main/java/com/tiviacz/travelersbackpack/client/screens/BackpackScreen.java
+++ src/main/java/com/tiviacz/travelersbackpack/client/screens/BackpackScreen.java
@@ -415,7 +415,7 @@ public class BackpackScreen extends AbstractContainerScreen<BackpackBaseMenu> im
     public void initButtons() {
         buttons.clear();
         int xOffset = 0;
-        if(getWrapper().getScreenID() == Reference.ITEM_SCREEN_ID) {
+        if(getWrapper().getScreenID() == Reference.ITEM_SCREEN_ID && getWrapper().getBackpackSlotIndex() == getScreenPlayer().getInventory().selected) {
             if(!TravelersBackpack.enableIntegration()) {
                 buttons.add(new EquipButton(this));
                 xOffset += 12;
@@ -424,7 +424,7 @@ public class BackpackScreen extends AbstractContainerScreen<BackpackBaseMenu> im
         if(getWrapper().getScreenID() == Reference.WEARABLE_SCREEN_ID && getWrapper().isOwner(getMenu().player)) {
             buttons.add(new MoreButton(this));
 
-            if(!TravelersBackpack.enableIntegration() && getWrapper().getBackpackSlotIndex() == getScreenPlayer().getInventory().selected) {
+            if(!TravelersBackpack.enableIntegration()) {
                 buttons.add(new UnequipButton(this));
                 xOffset += 12;
             }
