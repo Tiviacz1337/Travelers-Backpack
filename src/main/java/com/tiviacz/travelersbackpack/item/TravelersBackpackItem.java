94192c615d7f20f073f6c70870c90aa4975372dd
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2025-03-18 20:54:15 +0100

    🔧 Backpack cant be put in shulkerboxes

--- src/main/java/com/tiviacz/travelersbackpack/item/TravelersBackpackItem.java
+++ src/main/java/com/tiviacz/travelersbackpack/item/TravelersBackpackItem.java
@@ -307,7 +307,7 @@ public class TravelersBackpackItem extends BlockItem {
 
     @Override
     public boolean canFitInsideContainerItems() {
-        return TravelersBackpackConfig.getConfig().backpackSettings.allowShulkerBoxes;
+        return false;
     }
 
     public static void registerCauldronInteraction() {
