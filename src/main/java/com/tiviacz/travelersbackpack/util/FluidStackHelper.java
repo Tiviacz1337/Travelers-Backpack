4cbbf9c90e3f4c5dd754de38c1470caed2666f05
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2025-04-14 13:13:15 +0200

    🐛 Fixed compatibility with Amendments mod

--- src/main/java/com/tiviacz/travelersbackpack/util/FluidStackHelper.java
+++ src/main/java/com/tiviacz/travelersbackpack/util/FluidStackHelper.java
@@ -1,8 +1,8 @@
 package com.tiviacz.travelersbackpack.util;
 
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.item.alchemy.Potion;
 import net.minecraft.world.item.alchemy.PotionUtils;
 import net.minecraftforge.fluids.FluidStack;
 
@@ -19,19 +19,31 @@ public class FluidStackHelper {
         }
     }
 
-    public static Potion getPotionTypeFromFluidStack(FluidStack fluidStack) {
-        return PotionUtils.getPotion(fluidStack.getTag());
+    public static ItemStack getPotionStack(FluidStack fluidStack, Item potionItem) {
+        ItemStack potionStack = new ItemStack(potionItem);
+        if(fluidStack.hasTag()) {
+            if(fluidStack.getTag().contains(PotionUtils.TAG_POTION)) {
+                potionStack.getOrCreateTag().putString(PotionUtils.TAG_POTION, fluidStack.getTag().getString(PotionUtils.TAG_POTION));
+            }
+            if(fluidStack.getTag().contains(PotionUtils.TAG_CUSTOM_POTION_EFFECTS)) {
+                potionStack.getOrCreateTag().put(PotionUtils.TAG_CUSTOM_POTION_EFFECTS, fluidStack.getTag().getList(PotionUtils.TAG_CUSTOM_POTION_EFFECTS, 10));
+            }
+            if(fluidStack.getTag().contains(PotionUtils.TAG_CUSTOM_POTION_COLOR)) {
+                potionStack.getOrCreateTag().putInt(PotionUtils.TAG_CUSTOM_POTION_COLOR, fluidStack.getTag().getInt(PotionUtils.TAG_CUSTOM_POTION_COLOR));
+            }
+        }
+        return potionStack;
     }
 
     public static ItemStack getItemStackFromFluidStack(FluidStack fluidStack) {
-        return PotionUtils.setPotion(new ItemStack(Items.POTION), getPotionTypeFromFluidStack(fluidStack));
+        return getPotionStack(fluidStack, Items.POTION);
     }
 
     public static ItemStack getSplashItemStackFromFluidStack(FluidStack fluidStack) {
-        return PotionUtils.setPotion(new ItemStack(Items.SPLASH_POTION), getPotionTypeFromFluidStack(fluidStack));
+        return getPotionStack(fluidStack, Items.SPLASH_POTION);
     }
 
     public static ItemStack getLingeringItemStackFromFluidStack(FluidStack fluidStack) {
-        return PotionUtils.setPotion(new ItemStack(Items.LINGERING_POTION), getPotionTypeFromFluidStack(fluidStack));
+        return getPotionStack(fluidStack, Items.LINGERING_POTION);
     }
 }
