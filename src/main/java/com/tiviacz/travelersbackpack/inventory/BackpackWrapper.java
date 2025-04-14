cf2755eb2e11fd122b9c8f38823bf9a7512eac3e
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2025-04-14 14:27:33 +0200

    🐛 Fixed crash

--- src/main/java/com/tiviacz/travelersbackpack/inventory/BackpackWrapper.java
+++ src/main/java/com/tiviacz/travelersbackpack/inventory/BackpackWrapper.java
@@ -751,6 +751,7 @@ public class BackpackWrapper {
             }
             if(NbtHelper.has(stack, ModDataHelper.UPGRADE_TICK_INTERVAL)) {
                 int upgradeTicks = NbtHelper.get(stack, ModDataHelper.UPGRADE_TICK_INTERVAL);
+                if(upgradeTicks == 0) return;
                 BackpackWrapper wrapper;
                 if(ticks % upgradeTicks == 0) {
                     wrapper = ComponentUtils.getBackpackWrapper(player, stack, ComponentUtils.UPGRADES_ONLY);
