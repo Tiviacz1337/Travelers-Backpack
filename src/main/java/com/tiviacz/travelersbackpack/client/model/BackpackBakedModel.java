ff09b662f9037dbbaaf7d79f80eee3dc4b63f1bb
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2025-08-10 21:22:58 +0200

    🐛 Misc

--- src/main/java/com/tiviacz/travelersbackpack/client/model/BackpackBakedModel.java
+++ src/main/java/com/tiviacz/travelersbackpack/client/model/BackpackBakedModel.java
@@ -323,7 +323,7 @@ public class BackpackBakedModel implements BakedModel {
 
     @Override
     public ItemOverrides getOverrides() {
-        return ItemOverrides.EMPTY;
+        return this.bakedQuads.getBackpackBakedModel().getOverrides();
     }
 
     private static ItemTransforms createItemTransforms() {
