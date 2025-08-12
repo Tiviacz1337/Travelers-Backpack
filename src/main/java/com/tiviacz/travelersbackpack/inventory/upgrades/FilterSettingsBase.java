7ce1197d03f71ecc8518bac006237a4b55de90e1
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2025-08-12 20:41:14 +0200

    🔧 Fixed tags not updating

--- src/main/java/com/tiviacz/travelersbackpack/inventory/upgrades/FilterSettingsBase.java
+++ src/main/java/com/tiviacz/travelersbackpack/inventory/upgrades/FilterSettingsBase.java
@@ -32,17 +32,7 @@ public abstract class FilterSettingsBase {
         this.filterSettings = filterSettings;
         this.storage = storage;
         this.slotLimit = slotLimit;
-
-        if(this.filterTags != null) {
-            this.filterTags.forEach(string -> {
-                ResourceLocation tag = ResourceLocation.tryParse(string);
-                if(tag == null) return;
-                TagKey<Item> tagKey = TagKey.create(Registries.ITEM, tag);
-                if(!tags.contains(tagKey)) {
-                    tags.add(tagKey);
-                }
-            });
-        }
+        reloadItemTags();
     }
 
     public abstract boolean matchesFilter(@Nullable Player player, ItemStack stack);
@@ -69,6 +59,20 @@ public abstract class FilterSettingsBase {
         return arrayList.stream();
     }
 
+    public void reloadItemTags() {
+        this.tags.clear();
+        if(this.filterTags != null) {
+            this.filterTags.forEach(string -> {
+                ResourceLocation tag = ResourceLocation.tryParse(string);
+                if(tag == null) return;
+                TagKey<Item> tagKey = TagKey.create(Registries.ITEM, tag);
+                if(!tags.contains(tagKey)) {
+                    tags.add(tagKey);
+                }
+            });
+        }
+    }
+
     public void updateFilter(@Nullable List<ItemStack> items) {
         if(items == null) return;
         this.filterItems = items.stream().limit(this.slotLimit).filter(stack -> !stack.isEmpty()).toList();
@@ -77,6 +81,7 @@ public abstract class FilterSettingsBase {
     public void updateFilterTags(List<String> tags) {
         if(tags == null) return;
         this.filterTags = new ArrayList<>(tags);
+        reloadItemTags();
     }
 
     public void updateSettings(List<Integer> settings) {
