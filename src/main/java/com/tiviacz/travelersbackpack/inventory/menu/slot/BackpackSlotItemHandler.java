b8bb7cfd056e0bfbb0ddb33b21ce498c0cd0c56d
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2025-09-14 14:30:29 +0200

    🐛 Fixed EMI duplication bug

--- src/main/java/com/tiviacz/travelersbackpack/inventory/menu/slot/BackpackSlotItemHandler.java
+++ src/main/java/com/tiviacz/travelersbackpack/inventory/menu/slot/BackpackSlotItemHandler.java
@@ -35,6 +35,12 @@ public class BackpackSlotItemHandler extends SlotItemHandler {
         super.setChanged();
     }
 
+    @Override
+    public void onTake(Player player, ItemStack stack) {
+        set(getItem()); //Emi fix
+        super.onTake(player, stack);
+    }
+
     //Fixes JEI
     @Override
     public boolean mayPlace(ItemStack stack) {
