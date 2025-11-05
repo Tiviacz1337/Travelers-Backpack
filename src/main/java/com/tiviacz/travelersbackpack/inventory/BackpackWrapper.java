3822ad0448e2c735b7cc902c79726587dc2fe626
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2025-11-05 15:34:20 +0100

    ✨ Added new sorting methods

--- src/main/java/com/tiviacz/travelersbackpack/inventory/BackpackWrapper.java
+++ src/main/java/com/tiviacz/travelersbackpack/inventory/BackpackWrapper.java
@@ -14,6 +14,7 @@ import com.tiviacz.travelersbackpack.inventory.menu.BackpackBaseMenu;
 import com.tiviacz.travelersbackpack.inventory.menu.BackpackItemMenu;
 import com.tiviacz.travelersbackpack.inventory.menu.slot.BackpackSlotItemHandler;
 import com.tiviacz.travelersbackpack.inventory.menu.slot.ToolSlotItemHandler;
+import com.tiviacz.travelersbackpack.inventory.sorter.SortSelector;
 import com.tiviacz.travelersbackpack.inventory.upgrades.IEnable;
 import com.tiviacz.travelersbackpack.inventory.upgrades.ITickableUpgrade;
 import com.tiviacz.travelersbackpack.inventory.upgrades.tanks.TanksUpgrade;
@@ -385,6 +386,16 @@ public class BackpackWrapper {
 
     public boolean isAbilityEnabled() {
         return this.stack.getOrDefault(ModDataComponents.ABILITY_ENABLED, TravelersBackpackConfig.SERVER.backpackAbilities.forceAbilityEnabled.get());
+    }
+
+    public SortSelector.SortType getSortType() {
+        int type = this.stack.getOrDefault(ModDataComponents.SORT_TYPE, 0);
+        return SortSelector.SortType.values()[type];
+    }
+
+    public void setNextSortType() {
+        SortSelector.SortType type = getSortType();
+        setDataAndSync(ModDataComponents.SORT_TYPE.get(), type.next().ordinal());
     }
 
     public boolean hasSleepingBag() {
