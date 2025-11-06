976c37b392e73f3ecd3aa7d1dcf25b9ba1e0a4d0
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2025-11-06 19:44:56 +0100

    🐛 Fixed unequipping bug

--- src/main/java/com/tiviacz/travelersbackpack/common/ServerActions.java
+++ src/main/java/com/tiviacz/travelersbackpack/common/ServerActions.java
@@ -224,12 +224,27 @@ public class ServerActions {
 
         ItemStack backpack = CapabilityUtils.getWearingBackpack(player).copy();
 
-        if(!player.getInventory().add(backpack)) {
+        //Try to add to inventory
+        int index = player.getInventory().getSlotWithRemainingSpace(backpack);
+        if(index == -1) {
+            index = player.getInventory().getFreeSlot();
+        }
+
+        if(index != -1) {
+            player.getInventory().placeItemBackInInventory(backpack);
+        } else {
             if(player instanceof ServerPlayer serverPlayer) {
                 serverPlayer.sendSystemMessage(Component.translatable(Reference.NO_SPACE));
             }
             return false;
         }
+        
+        /*if(!player.getInventory().add(backpack)) {
+            if(player instanceof ServerPlayer serverPlayer) {
+                serverPlayer.sendSystemMessage(Component.translatable(Reference.NO_SPACE));
+            }
+            return false;
+        }*/
 
         CapabilityUtils.getCapability(player).ifPresent(attachment -> {
             attachment.equipBackpack(new ItemStack(Items.AIR, 0));
