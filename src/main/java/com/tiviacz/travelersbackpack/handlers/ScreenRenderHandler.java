5b9bb519624c86d2b873f73c13a6cff48b234f12
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2025-11-08 13:53:54 +0100

    🔧 Moved from unnecessary mixin to screen events

--- src/main/java/com/tiviacz/travelersbackpack/handlers/ScreenRenderHandler.java
+++ src/main/java/com/tiviacz/travelersbackpack/handlers/ScreenRenderHandler.java
@@ -1,20 +1,33 @@
 package com.tiviacz.travelersbackpack.handlers;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.tiviacz.travelersbackpack.TravelersBackpack;
 import com.tiviacz.travelersbackpack.client.screens.tooltip.BackpackTooltipComponent;
+import com.tiviacz.travelersbackpack.component.ComponentUtils;
+import com.tiviacz.travelersbackpack.config.TravelersBackpackConfig;
 import com.tiviacz.travelersbackpack.inventory.menu.slot.BackpackSlotItemHandler;
 import com.tiviacz.travelersbackpack.items.TravelersBackpackItem;
+import com.tiviacz.travelersbackpack.network.ServerboundActionTagPacket;
+import com.tiviacz.travelersbackpack.network.ServerboundRetrieveBackpackPacket;
+import com.tiviacz.travelersbackpack.util.PacketDistributorHelper;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
+import net.fabricmc.fabric.api.client.screen.v1.ScreenMouseEvents;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.gui.screens.inventory.EffectRenderingInventoryScreen;
+import net.minecraft.client.gui.screens.inventory.InventoryScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.inventory.tooltip.TooltipComponent;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import org.lwjgl.glfw.GLFW;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -57,6 +70,75 @@ public class ScreenRenderHandler {
                             }
                         }
                         guiGraphics.pose().popPose();
+                    }
+                });
+            }
+
+            if(screen2 instanceof InventoryScreen screen) {
+                ScreenEvents.afterRender(screen).register((screen1, guiGraphics, mouseX, mouseY, tickDelta) -> {
+                    //Render Backpack Icon if Backpack is equipped in Capability but Integration is enabled to easily retrieve the backpack
+                    if(Minecraft.getInstance().screen instanceof InventoryScreen && ComponentUtils.getComponentOptional(player).isPresent()) {
+                        if(ComponentUtils.getComponentOptional(player).get().hasBackpack() && TravelersBackpack.enableIntegration()) {
+                            ItemStack backpack = ComponentUtils.getComponentOptional(player).get().getBackpack();
+                            guiGraphics.renderItem(backpack, screen.leftPos + 77, screen.topPos + 62 - 18);
+
+                            if(mouseX >= screen.leftPos + 77 && mouseX < screen.leftPos + 77 + 16 && mouseY >= screen.topPos + 62 - 18 && mouseY < screen.topPos + 62 - 18 + 16) {
+                                AbstractContainerScreen.renderSlotHighlight(guiGraphics, screen.leftPos + 77, screen.topPos + 62 - 18, -1000);
+                                List<Component> components = new ArrayList<>();
+                                components.add(Component.translatable("screen.travelersbackpack.retrieve_backpack"));
+                                guiGraphics.renderTooltip(Minecraft.getInstance().font, components, Optional.of(new BackpackTooltipComponent(backpack)), mouseX, mouseY);
+                            }
+                        }
+                    }
+
+                    if(!TravelersBackpackConfig.getConfig().client.showBackpackIconInInventory) return;
+
+                    if(ComponentUtils.isWearingBackpack(player)) {
+                        if(TravelersBackpack.enableIntegration()) return;
+
+                        ItemStack backpack = ComponentUtils.getWearingBackpack(player);
+                        guiGraphics.renderItem(backpack, screen.leftPos + 77, screen.topPos + 62 - 18);
+
+                        if(mouseX >= screen.leftPos + 77 && mouseX < screen.leftPos + 77 + 16 && mouseY >= screen.topPos + 62 - 18 && mouseY < screen.topPos + 62 - 18 + 16) {
+                            EffectRenderingInventoryScreen.renderSlotHighlight(guiGraphics, screen.leftPos + 77, screen.topPos + 62 - 18, -1000);
+                            String button = KeybindHandler.OPEN_BACKPACK.getTranslatedKeyMessage().getString();
+                            List<Component> components = new ArrayList<>();
+                            components.add(Component.translatable("screen.travelersbackpack.open_inventory", button));
+                            components.add(Component.translatable("screen.travelersbackpack.unequip_tip"));
+                            components.add(Component.translatable("screen.travelersbackpack.hide_icon"));
+                            TooltipFlag.Default tooltipflag$default = Minecraft.getInstance().options.advancedItemTooltips ? TooltipFlag.Default.ADVANCED : TooltipFlag.Default.NORMAL;
+                            backpack.getItem().appendHoverText(backpack, player.level(), components, tooltipflag$default);
+                            guiGraphics.renderTooltip(Minecraft.getInstance().font, components, Optional.of(new BackpackTooltipComponent(backpack)), mouseX, mouseY);
+                        }
+                    }
+                });
+
+                ScreenMouseEvents.afterMouseClick(screen).register((screen1, mouseX, mouseY, button) -> {
+                    //Render Backpack Icon if Backpack is equipped in Capability but Integration is enabled to easily retrieve the backpack
+                    if(Minecraft.getInstance().screen instanceof InventoryScreen && ComponentUtils.getComponentOptional(player).isPresent()) {
+                        if(ComponentUtils.getComponentOptional(player).get().hasBackpack() && TravelersBackpack.enableIntegration()) {
+                            if(mouseX >= screen.leftPos + 77 && mouseX < screen.leftPos + 77 + 16 && mouseY >= screen.topPos + 62 - 18 && mouseY < screen.topPos + 62 - 18 + 16) {
+                                if(button == GLFW.GLFW_MOUSE_BUTTON_1) {
+                                    PacketDistributorHelper.sendToServer(new ServerboundRetrieveBackpackPacket(ComponentUtils.getComponentOptional(player).get().getBackpack().getItem().getDefaultInstance()));
+                                }
+                            }
+                        }
+                    }
+
+                    if(!TravelersBackpackConfig.getConfig().client.showBackpackIconInInventory) return;
+
+                    if(ComponentUtils.isWearingBackpack(player)) {
+                        if(TravelersBackpack.enableIntegration()) return;
+
+                        if(mouseX >= screen.leftPos + 77 && mouseX < screen.leftPos + 77 + 16 && mouseY >= screen.topPos + 62 - 18 && mouseY < screen.topPos + 62 - 18 + 16) {
+                            if(button == GLFW.GLFW_MOUSE_BUTTON_1) {
+                                if(InputConstants.isKeyDown(Minecraft.getInstance().getWindow().getWindow(), GLFW.GLFW_KEY_LEFT_SHIFT)) {
+                                    player.sendSystemMessage(Component.translatable("screen.travelersbackpack.hide_icon_info"));
+                                } else {
+                                    ServerboundActionTagPacket.create(ServerboundActionTagPacket.OPEN_SCREEN);
+                                }
+                            }
+                        }
                     }
                 });
             }
