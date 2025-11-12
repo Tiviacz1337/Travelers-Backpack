6c357e075c16ce0cdc1c397794924505b72a1e79
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2025-11-12 19:47:32 +0100

    🔧 Misc

--- src/main/java/com/tiviacz/travelersbackpack/mixin/ItemStackMixin.java
+++ src/main/java/com/tiviacz/travelersbackpack/mixin/ItemStackMixin.java
@@ -2,6 +2,7 @@ package com.tiviacz.travelersbackpack.mixin;
 
 import com.tiviacz.travelersbackpack.inventory.menu.AbstractBackpackMenu;
 import net.minecraft.world.entity.SlotAccess;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ClickAction;
 import net.minecraft.world.inventory.Slot;
@@ -17,7 +18,7 @@ public class ItemStackMixin {
     @Inject(method = "overrideOtherStackedOnMe", at = @At(value = "TAIL"))
     private void afterOverrideOtherStackedOnMe(ItemStack stack, Slot slot, ClickAction action, Player player, SlotAccess access, CallbackInfoReturnable<Boolean> cir) {
         boolean result = cir.getReturnValue();
-        if(player.containerMenu instanceof AbstractBackpackMenu) {
+        if(player.containerMenu instanceof AbstractBackpackMenu && !(slot.container instanceof Inventory)) { //Only for backpack, vanilla inventory slots work fine so do not include them
             if(result && (Object)this instanceof ItemStack clicked) {
                 slot.set(clicked.copy());
             }
