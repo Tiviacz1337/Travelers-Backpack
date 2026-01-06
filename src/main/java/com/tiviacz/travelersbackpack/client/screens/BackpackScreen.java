969a22dd20b84f1f2d68074026d62a7f0adf4bcc
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2026-01-06 23:54:47 +0100

    🔧 Naming changes

--- src/main/java/com/tiviacz/travelersbackpack/client/screens/BackpackScreen.java
+++ src/main/java/com/tiviacz/travelersbackpack/client/screens/BackpackScreen.java
@@ -72,7 +72,7 @@ public class BackpackScreen extends AbstractBackpackScreen<BackpackBaseMenu> imp
 
         //Reload screen
         updateScreen(true);
-        getMenu().updateSlots();
+        getMenu().rebuildSlots();
 
         //Update position
         updateBackpackSlotsPosition();
