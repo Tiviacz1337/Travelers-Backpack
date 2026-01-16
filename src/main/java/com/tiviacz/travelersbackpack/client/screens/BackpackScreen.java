19c5be3703cb0144dddedd0718dc1c62ad55ce43
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2026-01-16 13:56:58 +0100

    🐛 Hotfix for crafting upgrade

--- src/main/java/com/tiviacz/travelersbackpack/client/screens/BackpackScreen.java
+++ src/main/java/com/tiviacz/travelersbackpack/client/screens/BackpackScreen.java
@@ -21,6 +21,7 @@ import com.tiviacz.travelersbackpack.util.Reference;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.screens.inventory.MenuAccess;
 import net.minecraft.client.player.LocalPlayer;
@@ -378,6 +379,14 @@ public class BackpackScreen extends AbstractBackpackScreen<BackpackBaseMenu> imp
             return true;
         }
         return super.keyPressed(pKeyCode, pScanCode, pModifiers);
+    }
+
+    public void addCompatWidget(AbstractWidget widget) {
+        this.addRenderableWidget(widget);
+    }
+
+    public void removeCompatWidget(AbstractWidget widget) {
+        this.removeWidget(widget);
     }
 
     public static void displayTanksUpgradeWarning(Player player) {
