67ea2cbf824fc0bf6a60df9eb5f554a9557483cc
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2026-01-25 20:57:11 +0100

    ✨ Tool slots accept shields

--- src/main/java/com/tiviacz/travelersbackpack/inventory/menu/slot/ToolSlotItemHandler.java
+++ src/main/java/com/tiviacz/travelersbackpack/inventory/menu/slot/ToolSlotItemHandler.java
@@ -65,7 +65,8 @@ public class ToolSlotItemHandler extends SlotItemHandler {
                 stack.getItem() instanceof ProjectileWeaponItem ||
                 stack.getItem() instanceof BrushItem ||
                 stack.getItem() instanceof TridentItem ||
-                stack.getItem() instanceof SpyglassItem;
+                stack.getItem() instanceof SpyglassItem ||
+                stack.getItem() instanceof ShieldItem;
     }
 
     @Override
