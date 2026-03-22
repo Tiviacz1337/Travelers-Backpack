2f4d63942da8da2de0823b556f1f5deceb4e91b7
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2026-03-22 17:51:05 +0100

    🔧 Sleeping Bag appears directly under player

--- src/main/java/com/tiviacz/travelersbackpack/common/ServerActions.java
+++ src/main/java/com/tiviacz/travelersbackpack/common/ServerActions.java
@@ -371,7 +371,7 @@ public class ServerActions {
     public static void toggleSleepingBag(Player player, BlockPos pos, boolean isEquipped) {
         Level level = player.level();
         if(isEquipped) {
-            BlockPos sleepingBagPos1 = pos.relative(player.getDirection());
+            BlockPos sleepingBagPos1 = pos;
             BlockPos sleepingBagPos2 = sleepingBagPos1.relative(player.getDirection());
             boolean canPlace = placeAndUseSleepingBag(player, sleepingBagPos1, sleepingBagPos2, pos, level, player.getDirection());
             if(!canPlace) {
@@ -384,7 +384,7 @@ public class ServerActions {
 
             if(!level.isClientSide()) {
                 if(player instanceof ServerPlayer serverPlayer) {
-                    player.startSleepInBed(pos.relative(player.getDirection()).relative(player.getDirection())).ifLeft(bedSleepingProblem -> {
+                    player.startSleepInBed(pos.relative(player.getDirection())).ifLeft(bedSleepingProblem -> {
                         if(bedSleepingProblem.message() != null) {
                             player.displayClientMessage(bedSleepingProblem.message(), true);
                             if(level.getBlockState(sleepingBagPos1).getBlock() instanceof SleepingBagBlock) {
