dcfa47d95070ebd47692368d25289318a1cc5fab
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2026-03-26 00:01:08 +0100

    🔧 Misc

--- src/main/java/com/tiviacz/travelersbackpack/client/screens/BackpackScreen.java
+++ src/main/java/com/tiviacz/travelersbackpack/client/screens/BackpackScreen.java
@@ -440,6 +440,6 @@ public class BackpackScreen extends AbstractBackpackScreen<BackpackBaseMenu> imp
     }
 
     public Component createFluidWarning(FluidVariantWrapper fluidVariantWrapper, long backpackCapacity) {
-        return Component.literal(FluidTypeHelper.getFluidVariantName(fluidVariantWrapper.fluidVariant()).getString() + " " + fluidVariantWrapper.getViewAmount() + "/" + backpackCapacity + "mB").withStyle(ChatFormatting.RED);
+        return Component.literal(FluidTypeHelper.getFluidVariantName(fluidVariantWrapper.fluidVariant()).getString() + " " + fluidVariantWrapper.getViewAmount() + "/" + (backpackCapacity / 81) + "mB").withStyle(ChatFormatting.RED);
     }
 }
