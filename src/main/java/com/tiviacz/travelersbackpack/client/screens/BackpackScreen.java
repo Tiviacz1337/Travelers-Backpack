4cc9ba31c0b2e09706859ecea11727c25bc95c01
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2026-03-25 09:31:23 +0100

    🔧 Allow sorting via mouse buttons

--- src/main/java/com/tiviacz/travelersbackpack/client/screens/BackpackScreen.java
+++ src/main/java/com/tiviacz/travelersbackpack/client/screens/BackpackScreen.java
@@ -358,6 +358,11 @@ public class BackpackScreen extends AbstractBackpackScreen<BackpackBaseMenu> imp
                 b.mouseClicked(event, bl);
             }
         });
+        if(KeybindHandler.SORT_BACKPACK.matchesMouse(event)) {
+            ServerboundActionTagPacket.create(ServerboundActionTagPacket.SORTER, ContainerSorter.SORT_BACKPACK, KeyHelper.isShiftPressed());
+            playUIClickSound();
+            return true;
+        }
         return super.mouseClicked(event, bl);
     }
 
