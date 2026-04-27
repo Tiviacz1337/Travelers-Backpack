539a767afad79da26f8197d7819dcc0624948017
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2026-04-27 13:05:03 +0200

    🐛 Fixed issue that caused backpacks to not spawn on overworld entities

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,5 @@
 🐛 Fixed duplication issue with pickup upgrade  
+🐛 Fixed issue that caused backpacks to not spawn on overworld entities  
 
 ⭐ Want to receive **Supporter Star Badge** for a **lifetime**? - [visit my Ko-Fi page](https://ko-fi.com/tiviacz1337)!  
 ![Banner](https://i.imgur.com/SSrFv58.png)
--- src/main/java/com/tiviacz/travelersbackpack/config/TravelersBackpackConfig.java
+++ src/main/java/com/tiviacz/travelersbackpack/config/TravelersBackpackConfig.java
@@ -564,13 +564,15 @@ public class TravelersBackpackConfig {
             }
         }
 
-        public void loadEntityTypesFromConfig(List<? extends String> configList, List<EntityType> targetList) {
+        public void loadEntityTypesFromConfig(List<EntityType> targetList, List<? extends String>... configList) {
             targetList.clear();
-            for(String registryName : configList) {
-                ResourceLocation res = ResourceLocation.tryParse(registryName);
+            for(List<? extends String> list : configList) {
+                for(String registryName : list) {
+                    ResourceLocation res = ResourceLocation.tryParse(registryName);
 
-                if(BuiltInRegistries.ENTITY_TYPE.containsKey(res)) {
-                    targetList.add(BuiltInRegistries.ENTITY_TYPE.get(res));
+                    if(BuiltInRegistries.ENTITY_TYPE.containsKey(res)) {
+                        targetList.add(BuiltInRegistries.ENTITY_TYPE.get(res));
+                    }
                 }
             }
         }
@@ -651,8 +653,7 @@ public class TravelersBackpackConfig {
             loadItemsFromConfig(TravelersBackpackConfig.SERVER.backpackAbilities.allowedAbilities.get(), com.tiviacz.travelersbackpack.common.BackpackAbilities.ALLOWED_ABILITIES);
 
             //Entities
-            loadEntityTypesFromConfig(TravelersBackpackConfig.SERVER.world.possibleOverworldEntityTypes.get(), Reference.ALLOWED_TYPE_ENTRIES);
-            loadEntityTypesFromConfig(TravelersBackpackConfig.SERVER.world.possibleNetherEntityTypes.get(), Reference.ALLOWED_TYPE_ENTRIES);
+            loadEntityTypesFromConfig(Reference.ALLOWED_TYPE_ENTRIES, TravelersBackpackConfig.SERVER.world.possibleOverworldEntityTypes.get(), TravelersBackpackConfig.SERVER.world.possibleNetherEntityTypes.get());
 
             //Backpack Effects
             loadBackpackEffectsFromConfig(TravelersBackpackConfig.SERVER.backpackAbilities.backpackEffects.get(), com.tiviacz.travelersbackpack.common.BackpackAbilities.BACKPACK_EFFECTS);
