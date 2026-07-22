e6e3df3a1253755bb03316abed0226731d9b9d8c
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2026-07-22 16:33:17 +0200

    🔧 Backpack renders if equipped in Curios slot

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,4 @@
-🔧 Tools with enchantments will sort in proper order if sorted inside backpack  
-🐛 Backpacks no longer can be stored in tool slots  
-🐛 Fix for sleeping bag issue after returning to overworld  
-🐛 Fixed refmap error console log  
-📚 Updated russian translation - Thanks A1Asriel!  
-📚 Updated Vietnamese translation - Thanks Koshiisan!  
+🔧 Backpack renders if equipped in Curios slot  
 
 ⭐ Want to receive **Supporter Star Badge** for a **lifetime**? - [visit my Ko-Fi page](https://ko-fi.com/tiviacz1337)!  
 ![Banner](https://i.imgur.com/OF6652S.png)
--- src/main/java/com/tiviacz/travelersbackpack/compat/curios/TravelersBackpackCurioClient.java
+++ src/main/java/com/tiviacz/travelersbackpack/compat/curios/TravelersBackpackCurioClient.java
@@ -1,20 +1,37 @@
 package com.tiviacz.travelersbackpack.compat.curios;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.tiviacz.travelersbackpack.client.model.StackModelPart;
+import com.tiviacz.travelersbackpack.client.renderer.BackpackLayer;
+import com.tiviacz.travelersbackpack.init.ModItems;
+import com.tiviacz.travelersbackpack.item.TravelersBackpackItem;
+import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.model.player.PlayerModel;
+import net.minecraft.client.renderer.SubmitNodeCollector;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.client.renderer.entity.state.AvatarRenderState;
+import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
+import net.minecraft.client.renderer.item.ItemStackRenderState;
+import net.minecraft.world.item.ItemStack;
+import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.client.ICurioRenderer;
 
 public class TravelersBackpackCurioClient {
     public static void registerCurioRenderer() {
-        /*ModItems.ITEMS.getEntries().stream()
+        ModItems.ITEMS.getEntries().stream()
                 .filter(holder -> holder.get() instanceof TravelersBackpackItem)
-                .forEach(holder -> ICurioRenderer.register(holder.get(), TravelersBackpackCurioClient.Renderer::new));*/
+                .forEach(holder -> ICurioRenderer.register(holder.get(), TravelersBackpackCurioClient.Renderer::new));
     }
 
     public static class Renderer implements ICurioRenderer {
-        /*@Override
-        public <S extends LivingEntityRenderState, M extends EntityModel<? super S>> void render(ItemStack stack, SlotContext slotContext, PoseStack poseStack, @Nonnull MultiBufferSource renderTypeBuffer, int packedLight, S renderState, RenderLayerParent<S, M> renderLayerParent, EntityRendererProvider.Context context, float yRotation, float xRotation) {
+        @Override
+        public <S extends LivingEntityRenderState, M extends EntityModel<? super S>> void render(ItemStack stack, SlotContext slotContext, PoseStack poseStack, SubmitNodeCollector submitNodeCollector, int packedLight, S renderState, RenderLayerParent<S, M> renderLayerParent, EntityRendererProvider.Context context, float yRotation, float xRotation) {
             if(stack.getItem() instanceof TravelersBackpackItem && renderLayerParent.getModel() instanceof PlayerModel playerModel && renderState instanceof AvatarRenderState playerRenderState) {
-                BackpackLayer.renderBackpackLayer(playerModel, poseStack, renderTypeBuffer, packedLight, playerRenderState, stack);
+                var backpackRenderState = new ItemStackRenderState();
+                var tools = new StackModelPart();
+                BackpackLayer.renderBackpackLayer(playerModel, poseStack, submitNodeCollector, packedLight, playerRenderState, backpackRenderState, tools, stack);
             }
-        }*/
+        }
     }
 }
