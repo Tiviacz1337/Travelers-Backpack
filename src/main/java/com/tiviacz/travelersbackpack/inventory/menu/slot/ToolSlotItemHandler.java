29ab7f5583f187924720839bbd9794176384ab05
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2026-08-28 15:51:10 +0200

    🐛 Disabled ShulkerBoxes in Tool Slots

--- src/main/java/com/tiviacz/travelersbackpack/inventory/menu/slot/ToolSlotItemHandler.java
+++ src/main/java/com/tiviacz/travelersbackpack/inventory/menu/slot/ToolSlotItemHandler.java
@@ -40,6 +40,7 @@ public class ToolSlotItemHandler extends ResourceHandlerSlot {
     public static boolean isValid(ItemStack stack) {
         Item item = stack.getItem();
         if(item instanceof HoseItem) return false;
+        if(!item.canFitInsideContainerItems() || item instanceof BundleItem) return false;
 
         boolean isItemValid = BackpackSlotItemHandler.isItemValid(stack);
 
