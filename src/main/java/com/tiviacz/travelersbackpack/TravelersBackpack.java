a47f9af7c90087493dbff678e69c7815422075f7
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2026-08-29 01:47:25 +0200

    🔧 Tweaked scrolling functionality if Mouse Tweaks is installed

--- src/main/java/com/tiviacz/travelersbackpack/TravelersBackpack.java
+++ src/main/java/com/tiviacz/travelersbackpack/TravelersBackpack.java
@@ -42,6 +42,7 @@ public class TravelersBackpack implements ModInitializer {
     public static boolean polymorphLoaded;
     public static boolean trashSlotLoaded;
     public static boolean lambDynamicLightsLoaded;
+    public static boolean mouseTweaksLoaded;
 
     @Override
     public void onInitialize() {
@@ -109,6 +110,7 @@ public class TravelersBackpack implements ModInitializer {
         if(vinurlLoaded) VinURLNetwork.register();
 
         lambDynamicLightsLoaded = fabricLoader.isModLoaded("lambdynlights");
+        mouseTweaksLoaded = fabricLoader.isModLoaded("mousetweaks");
 
         //Fetch supporters
         Supporters.fetchSupporters();
