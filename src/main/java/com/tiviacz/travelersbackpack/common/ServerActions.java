8e47f0dc2077876950c93868cf9d0fa33ae9b338
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2026-08-29 01:41:17 +0200

    🐛 Fixed issue

--- src/main/java/com/tiviacz/travelersbackpack/common/ServerActions.java
+++ src/main/java/com/tiviacz/travelersbackpack/common/ServerActions.java
@@ -14,6 +14,7 @@ import com.tiviacz.travelersbackpack.inventory.handler.ItemStackHandler;
 import com.tiviacz.travelersbackpack.inventory.menu.BackpackBaseMenu;
 import com.tiviacz.travelersbackpack.inventory.menu.BackpackItemMenu;
 import com.tiviacz.travelersbackpack.inventory.menu.BackpackSettingsMenu;
+import com.tiviacz.travelersbackpack.inventory.menu.slot.FilterSlotItemHandler;
 import com.tiviacz.travelersbackpack.inventory.menu.slot.ToolSlotItemHandler;
 import com.tiviacz.travelersbackpack.inventory.sorter.ContainerSorter;
 import com.tiviacz.travelersbackpack.inventory.upgrades.IEnable;
@@ -38,6 +39,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.entity.projectile.throwableitemprojectile.ThrownLingeringPotion;
 import net.minecraft.world.entity.projectile.throwableitemprojectile.ThrownSplashPotion;
+import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -465,7 +467,16 @@ public class ServerActions {
 
         switch(type) {
             case SLOT: {
-                player.containerMenu.getSlot(index).set(stack);
+                if(index >= 0 && index < player.containerMenu.slots.size()) {
+                    Slot slot = player.containerMenu.getSlot(index);
+                    if(slot instanceof FilterSlotItemHandler filterSlot) {
+                        filterSlot.set(stack);
+                    }
+                    if(stack.isEmpty()) {
+                        player.containerMenu.getSlot(index).set(ItemStack.EMPTY);
+                        return;
+                    }
+                }
                 break;
             }
             case TANK: {
